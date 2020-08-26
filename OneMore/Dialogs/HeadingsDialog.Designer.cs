340cad1b890e396890950a716c599f6d662602a7
stevencohn <steven_cohn@waters.com>
2020-08-26 11:16:20 -0400

    Update HeadingDialog mockup

--- OneMore/Dialogs/HeadingsDialog.Designer.cs
+++ OneMore/Dialogs/HeadingsDialog.Designer.cs
@@ -40,6 +40,8 @@
 			this.indentationsGroup = new System.Windows.Forms.GroupBox();
 			this.indentTagBox = new System.Windows.Forms.CheckBox();
 			this.indentBox = new System.Windows.Forms.CheckBox();
+			this.tagButton = new System.Windows.Forms.Button();
+			this.tagLabel = new System.Windows.Forms.Label();
 			this.numberingGroup.SuspendLayout();
 			this.indentationsGroup.SuspendLayout();
 			this.SuspendLayout();
@@ -48,9 +50,10 @@
 			// 
 			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.okButton.Location = new System.Drawing.Point(283, 461);
+			this.okButton.Location = new System.Drawing.Point(175, 281);
+			this.okButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.okButton.Name = "okButton";
-			this.okButton.Size = new System.Drawing.Size(100, 38);
+			this.okButton.Size = new System.Drawing.Size(67, 25);
 			this.okButton.TabIndex = 9;
 			this.okButton.Text = "OK";
 			this.okButton.UseVisualStyleBackColor = true;
@@ -59,9 +62,10 @@
 			// 
 			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.Location = new System.Drawing.Point(389, 461);
+			this.cancelButton.Location = new System.Drawing.Point(245, 281);
+			this.cancelButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.cancelButton.Name = "cancelButton";
-			this.cancelButton.Size = new System.Drawing.Size(100, 38);
+			this.cancelButton.Size = new System.Drawing.Size(67, 25);
 			this.cancelButton.TabIndex = 8;
 			this.cancelButton.Text = "Cancel";
 			this.cancelButton.UseVisualStyleBackColor = true;
@@ -76,10 +80,10 @@
 			this.numberingGroup.Controls.Add(this.numDemoBox);
 			this.numberingGroup.Controls.Add(this.numRadio);
 			this.numberingGroup.Controls.Add(this.numberingBox);
-			this.numberingGroup.Location = new System.Drawing.Point(13, 18);
+			this.numberingGroup.Location = new System.Drawing.Point(9, 12);
+			this.numberingGroup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.numberingGroup.Name = "numberingGroup";
-			this.numberingGroup.Padding = new System.Windows.Forms.Padding(5);
-			this.numberingGroup.Size = new System.Drawing.Size(476, 224);
+			this.numberingGroup.Size = new System.Drawing.Size(303, 146);
 			this.numberingGroup.TabIndex = 11;
 			this.numberingGroup.TabStop = false;
 			this.numberingGroup.Text = "Numbering";
@@ -87,22 +91,25 @@
 			// cleanBox
 			// 
 			this.cleanBox.AutoSize = true;
-			this.cleanBox.Location = new System.Drawing.Point(25, 172);
+			this.cleanBox.Location = new System.Drawing.Point(17, 112);
+			this.cleanBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.cleanBox.Name = "cleanBox";
-			this.cleanBox.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
-			this.cleanBox.Size = new System.Drawing.Size(230, 34);
+			this.cleanBox.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+			this.cleanBox.Size = new System.Drawing.Size(199, 23);
 			this.cleanBox.TabIndex = 14;
-			this.cleanBox.Text = "Remove existing numbering";
+			this.cleanBox.Text = "Remove/cleanup existing numbering";
 			this.cleanBox.UseVisualStyleBackColor = true;
 			// 
 			// alphaDemoBox
 			// 
 			this.alphaDemoBox.BackColor = System.Drawing.SystemColors.Window;
-			this.alphaDemoBox.Location = new System.Drawing.Point(83, 99);
+			this.alphaDemoBox.Enabled = false;
+			this.alphaDemoBox.Location = new System.Drawing.Point(55, 64);
+			this.alphaDemoBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.alphaDemoBox.Multiline = true;
 			this.alphaDemoBox.Name = "alphaDemoBox";
 			this.alphaDemoBox.ReadOnly = true;
-			this.alphaDemoBox.Size = new System.Drawing.Size(128, 67);
+			this.alphaDemoBox.Size = new System.Drawing.Size(87, 45);
 			this.alphaDemoBox.TabIndex = 12;
 			this.alphaDemoBox.TabStop = false;
 			this.alphaDemoBox.Text = "1.\r\n   a.\r\n      i.";
@@ -110,9 +117,11 @@
 			// alphaRadio
 			// 
 			this.alphaRadio.AutoSize = true;
-			this.alphaRadio.Location = new System.Drawing.Point(55, 69);
+			this.alphaRadio.Enabled = false;
+			this.alphaRadio.Location = new System.Drawing.Point(37, 45);
+			this.alphaRadio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.alphaRadio.Name = "alphaRadio";
-			this.alphaRadio.Size = new System.Drawing.Size(136, 24);
+			this.alphaRadio.Size = new System.Drawing.Size(92, 17);
 			this.alphaRadio.TabIndex = 0;
 			this.alphaRadio.TabStop = true;
 			this.alphaRadio.Text = "Alpha-numeric";
@@ -121,11 +130,13 @@
 			// numDemoBox
 			// 
 			this.numDemoBox.BackColor = System.Drawing.SystemColors.Window;
-			this.numDemoBox.Location = new System.Drawing.Point(315, 99);
+			this.numDemoBox.Enabled = false;
+			this.numDemoBox.Location = new System.Drawing.Point(193, 64);
+			this.numDemoBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.numDemoBox.Multiline = true;
 			this.numDemoBox.Name = "numDemoBox";
 			this.numDemoBox.ReadOnly = true;
-			this.numDemoBox.Size = new System.Drawing.Size(128, 67);
+			this.numDemoBox.Size = new System.Drawing.Size(87, 45);
 			this.numDemoBox.TabIndex = 13;
 			this.numDemoBox.TabStop = false;
 			this.numDemoBox.Text = "1.\r\n   1.1.\r\n      1.1.1.";
@@ -133,9 +144,11 @@
 			// numRadio
 			// 
 			this.numRadio.AutoSize = true;
-			this.numRadio.Location = new System.Drawing.Point(287, 69);
+			this.numRadio.Enabled = false;
+			this.numRadio.Location = new System.Drawing.Point(174, 45);
+			this.numRadio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.numRadio.Name = "numRadio";
-			this.numRadio.Size = new System.Drawing.Size(92, 24);
+			this.numRadio.Size = new System.Drawing.Size(64, 17);
 			this.numRadio.TabIndex = 1;
 			this.numRadio.TabStop = true;
 			this.numRadio.Text = "Numeric";
@@ -144,10 +157,11 @@
 			// numberingBox
 			// 
 			this.numberingBox.AutoSize = true;
-			this.numberingBox.Location = new System.Drawing.Point(25, 34);
+			this.numberingBox.Location = new System.Drawing.Point(17, 22);
+			this.numberingBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.numberingBox.Name = "numberingBox";
-			this.numberingBox.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
-			this.numberingBox.Size = new System.Drawing.Size(145, 29);
+			this.numberingBox.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+			this.numberingBox.Size = new System.Drawing.Size(99, 20);
 			this.numberingBox.TabIndex = 10;
 			this.numberingBox.Text = "Add Numbering";
 			this.numberingBox.UseVisualStyleBackColor = true;
@@ -157,11 +171,15 @@
 			// 
 			this.indentationsGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.indentationsGroup.Controls.Add(this.tagLabel);
+			this.indentationsGroup.Controls.Add(this.tagButton);
 			this.indentationsGroup.Controls.Add(this.indentTagBox);
 			this.indentationsGroup.Controls.Add(this.indentBox);
-			this.indentationsGroup.Location = new System.Drawing.Point(13, 248);
+			this.indentationsGroup.Location = new System.Drawing.Point(9, 161);
+			this.indentationsGroup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.indentationsGroup.Name = "indentationsGroup";
-			this.indentationsGroup.Size = new System.Drawing.Size(476, 160);
+			this.indentationsGroup.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.indentationsGroup.Size = new System.Drawing.Size(303, 104);
 			this.indentationsGroup.TabIndex = 12;
 			this.indentationsGroup.TabStop = false;
 			this.indentationsGroup.Text = "Indentations";
@@ -169,9 +187,11 @@
 			// indentTagBox
 			// 
 			this.indentTagBox.AutoSize = true;
-			this.indentTagBox.Location = new System.Drawing.Point(25, 65);
+			this.indentTagBox.Enabled = false;
+			this.indentTagBox.Location = new System.Drawing.Point(17, 48);
+			this.indentTagBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.indentTagBox.Name = "indentTagBox";
-			this.indentTagBox.Size = new System.Drawing.Size(236, 24);
+			this.indentTagBox.Size = new System.Drawing.Size(160, 17);
 			this.indentTagBox.TabIndex = 16;
 			this.indentTagBox.Text = "Indent only tagged headings";
 			this.indentTagBox.UseVisualStyleBackColor = true;
@@ -179,30 +199,51 @@
 			// indentBox
 			// 
 			this.indentBox.AutoSize = true;
-			this.indentBox.Location = new System.Drawing.Point(25, 25);
+			this.indentBox.Location = new System.Drawing.Point(17, 16);
+			this.indentBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.indentBox.Name = "indentBox";
-			this.indentBox.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
-			this.indentBox.Size = new System.Drawing.Size(245, 34);
+			this.indentBox.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+			this.indentBox.Size = new System.Drawing.Size(167, 23);
 			this.indentBox.TabIndex = 15;
 			this.indentBox.Text = "Indent content below heading";
 			this.indentBox.UseVisualStyleBackColor = true;
 			// 
+			// tagButton
+			// 
+			this.tagButton.Enabled = false;
+			this.tagButton.Location = new System.Drawing.Point(69, 70);
+			this.tagButton.Name = "tagButton";
+			this.tagButton.Size = new System.Drawing.Size(40, 23);
+			this.tagButton.TabIndex = 17;
+			this.tagButton.Text = "?";
+			this.tagButton.UseVisualStyleBackColor = true;
+			// 
+			// tagLabel
+			// 
+			this.tagLabel.AutoSize = true;
+			this.tagLabel.Location = new System.Drawing.Point(34, 75);
+			this.tagLabel.Name = "tagLabel";
+			this.tagLabel.Size = new System.Drawing.Size(29, 13);
+			this.tagLabel.TabIndex = 18;
+			this.tagLabel.Text = "Tag:";
+			// 
 			// HeadingsDialog
 			// 
 			this.AcceptButton = this.okButton;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(502, 512);
+			this.ClientSize = new System.Drawing.Size(321, 314);
 			this.Controls.Add(this.indentationsGroup);
 			this.Controls.Add(this.numberingGroup);
 			this.Controls.Add(this.okButton);
 			this.Controls.Add(this.cancelButton);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "HeadingsDialog";
-			this.Padding = new System.Windows.Forms.Padding(10, 15, 10, 10);
+			this.Padding = new System.Windows.Forms.Padding(7, 10, 7, 6);
 			this.ShowInTaskbar = false;
 			this.Text = "Headings";
 			this.numberingGroup.ResumeLayout(false);
@@ -227,5 +268,7 @@
 		private System.Windows.Forms.GroupBox indentationsGroup;
 		private System.Windows.Forms.CheckBox indentTagBox;
 		private System.Windows.Forms.CheckBox indentBox;
+		private System.Windows.Forms.Label tagLabel;
+		private System.Windows.Forms.Button tagButton;
 	}
 }
