72e3049b63c1b9d9060fc155ef8ba35757432700
Steven M. Cohn <steven_cohn@waters.com>
2020-12-12 10:26:13 -0500

    cleanup

--- OneMore/Commands/Edit/ColorizeCommand.cs
+++ OneMore/Commands/Edit/ColorizeCommand.cs
@@ -23,54 +23,56 @@ namespace River.OneMoreAddIn.Commands
 
 			using (var one = new OneNote(out var page, out var ns))
 			{
-				var updated = false;
-
 				var runs = page.Root.Descendants(ns + "T")
 					.Where(e => e.Attributes().Any(a => a.Name == "selected" && a.Value == "all"));
 
-				if (runs != null)
+				if (runs == null)
 				{
-					foreach (var run in runs.ToList())
+					return;
+				}
+
+				var updated = false;
+
+				foreach (var run in runs.ToList())
+				{
+					var cdata = run.GetCData();
+
+					if (cdata.Value.Contains("<br>"))
 					{
-						var cdata = run.GetCData();
+						// special handling to expand soft line breaks (Shift + Enter) into
+						// hard breaks, splitting the line into multiple ines...
 
-						if (cdata.Value.Contains("<br>"))
+						var text = cdata.GetWrapper().Value;
+						var lines = text.Split(new char[] { '\n' });
+
+						// update current cdata with first line
+						cdata.Value = colorizer.ColorizeOne(lines[0]);
+
+						// collect subsequent lines from soft-breaks
+						var elements = new List<XElement>();
+						for (int i = 1; i < lines.Length; i++)
 						{
-							// special handling to expand soft line breaks (Shift + Enter) and
-							// split the line into multiple ines...
-
-							var text = cdata.GetWrapper().Value;
-							var lines = text.Split(new char[] { '\n' });
-
-							// update current cdata with first line
-							cdata.Value = colorizer.ColorizeOne(lines[0]);
-
-							// collect subsequent lines from soft-breaks
-							var elements = new List<XElement>();
-							for (int i = 1; i < lines.Length; i++)
-							{
-								elements.Add(new XElement(ns + "OE",
-									run.Parent.Attributes(),
-									new XElement(ns + "T",
-										new XCData(colorizer.ColorizeOne(lines[i]))
-									)));
-							}
-
-							run.Parent.AddAfterSelf(elements);
-
-							updated = true;
+							elements.Add(new XElement(ns + "OE",
+								run.Parent.Attributes(),
+								new XElement(ns + "T",
+									new XCData(colorizer.ColorizeOne(lines[i]))
+								)));
 						}
-						else
-						{
-							cdata.Value = colorizer.ColorizeOne(cdata.GetWrapper().Value);
-							updated = true;
-						}
+
+						run.Parent.AddAfterSelf(elements);
+
+						updated = true;
 					}
-
-					if (updated)
+					else
 					{
-						one.Update(page);
+						cdata.Value = colorizer.ColorizeOne(cdata.GetWrapper().Value);
+						updated = true;
 					}
+				}
+
+				if (updated)
+				{
+					one.Update(page);
 				}
 			}
 		}
