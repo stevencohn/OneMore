2e52fd877b1776ad536b01a71599ff4589ce9895
Steven M. Cohn <steven_cohn@waters.com>
2020-12-16 18:22:57 -0500

    Fix Colorizer to handle soft-breaks better

--- OneMore/Commands/Edit/ColorizeCommand.cs
+++ OneMore/Commands/Edit/ColorizeCommand.cs
@@ -45,20 +45,23 @@ namespace River.OneMoreAddIn.Commands
 						// special handling to expand soft line breaks (Shift + Enter) into
 						// hard breaks, splitting the line into multiple ines...
 
-						var text = cdata.GetWrapper().Value;
+						var text = cdata.Value;
 						var lines = text.Split(new string[] { "<br>" }, StringSplitOptions.None);
 
 						// update current cdata with first line
-						cdata.Value = colorizer.ColorizeOne(lines[0]);
+						cdata.Value = colorizer.ColorizeOne(
+							new XCData(lines[0]).GetWrapper().Value);
 
 						// collect subsequent lines from soft-breaks
 						var elements = new List<XElement>();
 						for (int i = 1; i < lines.Length; i++)
 						{
+							var colorized = colorizer.ColorizeOne(
+								new XCData(lines[i]).GetWrapper().Value);
+
 							elements.Add(new XElement(ns + "OE",
 								run.Parent.Attributes(),
-								new XElement(ns + "T",
-									new XCData(colorizer.ColorizeOne(lines[i]))
+								new XElement(ns + "T", new XCData(colorized)
 								)));
 						}
 
@@ -81,3 +84,11 @@ namespace River.OneMoreAddIn.Commands
 		}
 	}
 }
+/*
+<one:T><![CDATA[&lt;?xml version=&quot;1.0&quot;?&gt; <br>
+&lt;hello xmlns=&quot;urn:ietf:params:xml:ns:netconf:base:1.0&quot;&gt; <br>
+  &lt;capabilities&gt;   <br>
+    &lt;capability&gt;urn:ietf:params:xml:ns:netconf:base:1.0&lt;/capability&gt;   <br>
+  &lt;/capabilities&gt;   <br>
+&lt;/hello&gt;]]&gt;]]&gt;]]></one:T>
+*/
