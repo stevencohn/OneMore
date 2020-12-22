ca19cf838fa4fa4298ae2c2674062e6b515d7209
Steven M. Cohn <steven_cohn@waters.com>
2020-12-22 13:29:32 -0500

    indents

--- OneMore/Commands/Extras/MapCommand.cs
+++ OneMore/Commands/Extras/MapCommand.cs
@@ -17,7 +17,6 @@ namespace River.OneMoreAddIn.Commands
 	{
 
 		private readonly Regex regex;
-		private readonly Dictionary<string, string> titles;
 
 		private OneNote one;
 		private OneNote.Scope scope;
@@ -29,8 +28,6 @@ namespace River.OneMoreAddIn.Commands
 			// internal hyperlinks are within CDATA similar to: <a href="onenote:#three&amp;
 			//   section-id={..}&amp;page-id={..}&amp;end&amp;base-path=https://.."
 			regex = new Regex(@"page-id=({[^}]+?})");
-
-			titles = new Dictionary<string, string>();
 		}
 
 
@@ -54,6 +51,8 @@ namespace River.OneMoreAddIn.Commands
 		private void Execute(UI.ProgressDialog progress, CancellationToken token)
 		{
 			logger.StartClock();
+
+			var titles = new Dictionary<string, string>();
 
 			using (one = new OneNote())
 			{
@@ -223,21 +222,23 @@ namespace River.OneMoreAddIn.Commands
 
 			var container = page.EnsureContentContainer();
 
-			BuildMapPage(hierarchy, page, container);
+			container.Add(BuildMapPage(hierarchy, page));
 
 			one.Update(page);
 			one.NavigateTo(pageId);
 		}
 
 
-		private void BuildMapPage(XElement element, Page page, XElement container)
+		private IEnumerable<XElement> BuildMapPage(XElement element, Page page)
 		{
+			var content = new List<XElement>();
+
 			if (element.Name.LocalName == "Page")
 			{
 				var pname = element.Attribute("name").Value;
 				var plink = one.GetHyperlink(element.Attribute("ID").Value, string.Empty);
 
-				container.Add(new XElement(ns + "OE",
+				content.Add(new XElement(ns + "OE",
 					new XElement(ns + "T",
 						new XCData($"<a href=\"{plink}\">{pname}</a>")
 					)));
@@ -247,7 +248,7 @@ namespace River.OneMoreAddIn.Commands
 					pname = reference.Attribute("title").Value;
 					plink = one.GetHyperlink(reference.Attribute("ID").Value, string.Empty);
 
-					container.Add(new XElement(ns + "OE",
+					content.Add(new XElement(ns + "OE",
 						new XElement(ns + "T",
 							new XCData($". . <a href=\"{plink}\">{pname}</a>")
 						)));
@@ -270,17 +271,21 @@ namespace River.OneMoreAddIn.Commands
 					index = MakeQuickStyle(page, StandardStyles.Heading1);
 				}
 
-				container.Add(new XElement(ns + "OE",
-					new XAttribute("quickStyleIndex", index.ToString()),
-					new XElement(ns + "T",
-					new XCData(element.Attribute("name").Value)
-					)));
+				var indents = new XElement(ns + "OEChildren");
 
 				foreach (var child in element.Elements())
 				{
-					BuildMapPage(child, page, container);
+					indents.Add(BuildMapPage(child, page));
 				}
+
+				content.Add(new XElement(ns + "OE",
+					new XAttribute("quickStyleIndex", index.ToString()),
+					new XElement(ns + "T", new XCData(element.Attribute("name").Value)),
+					indents
+					));
 			}
+
+			return content;
 		}
 
 
@@ -304,27 +309,3 @@ namespace River.OneMoreAddIn.Commands
 		}
 	}
 }
-/*
-<one:Section xmlns:one="http://schemas.microsoft.com/office/onenote/2013/onenote" name="OneMore" ID="{B49560C5-7EEF-41CB-9B62-A1999A367EC1}{1}{B0}" path="https://d.docs.live.net/6925d0374517d4b4/Documents/Personal/OneMore.one" lastModifiedTime="2020-12-22T17:06:54.000Z" color="#B49EDE" isCurrentlyViewed="true">
-  <one:Page ID="{B49560C5-7EEF-41CB-9B62-A1999A367EC1}{1}{E1954536723328074054651949015119196467408721}" name="To avoid any issues, outstanding changes must be discarded when the lock is releasedâ€”either implicitly on â€œ session failureâ€ or explicitly by using the unlock operation." dateTime="2020-12-19T13:48:40.000Z" lastModifiedTime="2020-12-22T17:06:54.000Z" pageLevel="1">
-    <one:Meta name="omHighlightIndex" content="1" />
-    <Ref title="Play" pageId="{B49560C5-7EEF-41CB-9B62-A1999A367EC1}{1}{E17811441452697988761986790585466670065101}" />
-  </one:Page>
-</one:Section>
-
-<one:Notebook xmlns:one="http://schemas.microsoft.com/office/onenote/2013/onenote" name="Personal" nickname="Personal" ID="{CAE56365-6026-4E6C-A313-667D6FEBE5D8}{1}{B0}" path="https://d.docs.live.net/6925d0374517d4b4/Documents/Personal/" lastModifiedTime="2020-12-22T00:15:48.000Z" color="#F6B078" isCurrentlyViewed="true">
-  <one:Section name="Hofstra" ID="{657DD8C1-23A4-4733-9009-BCECF5857CA5}{1}{B0}" path="https://d.docs.live.net/6925d0374517d4b4/Documents/Personal/Hofstra.one" lastModifiedTime="2020-12-11T11:28:03.000Z" color="#B49EDE">
-    <one:Page ID="{657DD8C1-23A4-4733-9009-BCECF5857CA5}{1}{E1953424436810837733961954981927889815044431}" name="Hofstra" dateTime="2017-02-02T23:15:19.000Z" lastModifiedTime="2020-12-11T11:28:03.000Z" pageLevel="1">
-      <Ref title="529 Confirmation 9/4/2020" pageId="{657DD8C1-23A4-4733-9009-BCECF5857CA5}{1}{E19518009081657497934320169331363838181454091}" />
-      <Ref title="529 Confirmation 2/1/2020" pageId="{657DD8C1-23A4-4733-9009-BCECF5857CA5}{1}{E1946875086941500500191984307843079045118961}" />
-    </one:Page>
-  </one:Section>
-
-<one:Notebooks xmlns:one="http://schemas.microsoft.com/office/onenote/2013/onenote">
-  <one:Notebook name="Personal" nickname="Personal" ID="{CAE56365-6026-4E6C-A313-667D6FEBE5D8}{1}{B0}" path="https://d.docs.live.net/6925d0374517d4b4/Documents/Personal/" lastModifiedTime="2020-12-22T17:06:54.000Z" color="#F6B078" isCurrentlyViewed="true">
-    <one:Section name="Notes" ID="{414F807D-E5C7-041C-1513-376CEB04177B}{1}{B0}" path="https://d.docs.live.net/6925d0374517d4b4/Documents/Personal/Notes.one" lastModifiedTime="2020-11-14T12:03:10.000Z" color="#F6B078">
-      <one:Page ID="{414F807D-E5C7-041C-1513-376CEB04177B}{1}{E19508546433742256804920172178273817917930781}" name="iDevice, Kindle, Surface" dateTime="2011-04-28T23:44:17.000Z" lastModifiedTime="2020-07-13T16:19:32.000Z" pageLevel="1" isCollapsed="true">
-        <Ref title="iPhone" pageId="{19D2987D-72BD-0D29-17FD-7D30C15F1FE2}{1}{E19476019463238140933820182814637071124832001}" />
-      </one:Page>
-    </one:Section>
- */
