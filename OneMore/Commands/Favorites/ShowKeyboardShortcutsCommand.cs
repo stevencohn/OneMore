79e5d3c505f4bf2740cdc9a0500af3e642aefcd3
Steven Cohn <stevencohn@live.com>
2020-12-28 08:12:34 -0500

    keyboard shortcuts

--- OneMore/Commands/Favorites/ShowKeyboardShortcutsCommand.cs
+++ OneMore/Commands/Favorites/ShowKeyboardShortcutsCommand.cs
@@ -6,6 +6,7 @@
 
 namespace River.OneMoreAddIn.Commands
 {
+	using River.OneMoreAddIn.Models;
 	using System;
 	using System.IO;
 	using System.IO.Compression;
@@ -13,11 +14,14 @@ namespace River.OneMoreAddIn.Commands
 	using System.Net;
 	using System.Net.Http;
 	using System.Threading.Tasks;
+	using System.Xml.Linq;
 
 	internal class ShowKeyboardShortcutsCommand : Command
 	{
 		private const string TemplateUrl =
 			"https://github.com/stevencohn/OneMore/raw/master/Templates/OneNote_Keyboard_Shortcuts.zip";
+
+		private OneNote one;
 
 
 		public ShowKeyboardShortcutsCommand()
@@ -27,7 +31,7 @@ namespace River.OneMoreAddIn.Commands
 
 		public override async void Execute(params object[] args)
 		{
-			using (var one = new OneNote())
+			using (one = new OneNote())
 			{
 				var results = one.SearchMeta(string.Empty, "omKeyboardShortcuts");
 				var ns = one.GetNamespace(results);
@@ -50,6 +54,10 @@ namespace River.OneMoreAddIn.Commands
 				{
 					one.NavigateTo(pageId);
 				}
+				else
+				{
+					UIHelper.ShowMessage("Could not download or import template, see log file for details");
+				}
 			}
 		}
 
@@ -68,26 +76,64 @@ namespace River.OneMoreAddIn.Commands
 			{
 				using (var response = await client.GetAsync(TemplateUrl))
 				{
-					using (var dstream = new DeflateStream(
-						await response.Content.ReadAsStreamAsync(),
-						CompressionMode.Decompress))
+					var path = Path.GetTempFileName();
+					using (var output = File.Create(path))
 					{
-						var path = Path.GetTempFileName();
-						using (var output = File.Create(path))
+						await response.Content.CopyToAsync(output);
+					}
+
+					using (var fs = new FileStream(path, FileMode.Open))
+					using (var zip = new ZipArchive(fs))
+					{
+						var entry = zip.Entries.First();
+
+						using (var sr = new StreamReader(entry.Open()))
 						{
-							await dstream.CopyToAsync(output);
-							logger.WriteLine(path);
+							pageId = ImportTemplate(new Page(XElement.Parse(sr.ReadToEnd())));
 						}
+					}
+
+					if (File.Exists(path))
+					{
+						File.Delete(path);
 					}
 				}
 			}
 			catch (Exception exc)
 			{
 				logger.WriteLine("error downloading shortcuts template", exc);
-				return null;
 			}
 
 			return pageId;
+		}
+
+
+		private string ImportTemplate(Page template)
+		{
+			try
+			{
+				one.CreatePage(one.CurrentSectionId, out var pageId);
+
+				// remove any objectID values and let OneNote generate new IDs
+				template.Root.Descendants().Attributes("objectID").Remove();
+
+				// set the page ID to the new page's ID
+				template.Root.Attribute("ID").Value = pageId;
+
+				if (string.IsNullOrEmpty(template.Title))
+				{
+					template.Title = "OneNote Keyboard Shortcuts";
+				}
+
+				one.Update(template);
+
+				return pageId;
+			}
+			catch (Exception exc)
+			{
+				logger.WriteLine("error importing template", exc);
+				return null;
+			}
 		}
 	}
 }
