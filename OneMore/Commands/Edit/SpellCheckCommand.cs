f8446e0f91c1c409d664528a98e93dcf9d3bc056
Steven Cohn <stevencohn@live.com>
2021-04-10 09:55:16 -0400

    Changed spell check command to recognize selected text regions, #216

--- OneMore/Commands/Edit/SpellCheckCommand.cs
+++ OneMore/Commands/Edit/SpellCheckCommand.cs
@@ -4,6 +4,8 @@
 
 namespace River.OneMoreAddIn.Commands
 {
+	using System.Linq;
+	using System.Threading;
 	using System.Threading.Tasks;
 	using System.Xml.Linq;
 
@@ -25,18 +27,40 @@ namespace River.OneMoreAddIn.Commands
 			{
 				if (page != null)
 				{
-					// remove all occurances of the "lang=" attribute across the entire page
-
-					page.Root.DescendantsAndSelf()
-						.Attributes("lang")
-						.Remove();
-
-					if (disable)
+					if (page.GetTextCursor() == null)
 					{
-						// set lang=yo for the page and the page title
+						// update only selected text...
 
-						page.Root.Add(new XAttribute("lang", "yo"));
-						page.Root.Element(ns + "Title")?.Add(new XAttribute("lang", "yo"));
+						if (disable)
+						{
+							page.Root.Elements(ns + "Outline")
+								.Descendants(ns + "T")
+								.Where(e => e.Attributes("selected").Any(a => a.Value.Equals("all")))
+								.ForEach(e => e.SetAttributeValue("lang", "yo"));
+						}
+						else
+						{
+							page.Root.Elements(ns + "Outline")
+								.Descendants(ns + "T")
+								.Where(e => e.Attributes("selected").Any(a => a.Value.Equals("all")))
+								.ForEach(e => e.SetAttributeValue("lang", Thread.CurrentThread.CurrentUICulture.Name));
+						}
+					}
+					else
+					{
+						// remove all occurances of the "lang=" attribute across the entire page
+
+						page.Root.DescendantsAndSelf()
+							.Attributes("lang")
+							.Remove();
+
+						if (disable)
+						{
+							// set lang=yo for the page and the page title
+
+							page.Root.Add(new XAttribute("lang", "yo"));
+							page.Root.Element(ns + "Title")?.Add(new XAttribute("lang", "yo"));
+						}
 					}
 
 					await one.Update(page);
