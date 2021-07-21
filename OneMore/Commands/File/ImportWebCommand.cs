a2108c29bef34318b04e50189d97bba80023f518
Steven Cohn <stevencohn@live.com>
2021-07-21 18:01:15 -0400

    import web page PoC

--- OneMore/Commands/File/ImportWebCommand.cs
+++ OneMore/Commands/File/ImportWebCommand.cs
@@ -7,10 +7,9 @@ namespace River.OneMoreAddIn.Commands
 	using HtmlAgilityPack;
 	using System;
 	using System.Linq;
-	using System.Net;
 	using System.Text.RegularExpressions;
+	using System.Threading;
 	using System.Threading.Tasks;
-	using System.Xml.Linq;
 
 	internal class ImportWebCommand : Command
 	{
@@ -21,87 +20,103 @@ namespace River.OneMoreAddIn.Commands
 
 		public override async Task Execute(params object[] args)
 		{
-			ServicePointManager.SecurityProtocol =
-				SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+			logger.StartClock();
 
 			var baseUri = new Uri("https://github.com/stevencohn/OneMore/wiki");
+			var client = HttpClientFactory.Create();
 
-			using (var client = new WebClient())
+			// download html
+
+			string content = null;
+
+			try
 			{
-				// download html
-
-				var content = client.DownloadString(baseUri);
-				var doc = new HtmlDocument();
-				doc.LoadHtml(content);
-
-				// convert img tags to anchor tags
-				// OneNote will remove img tags but will keep anchors
-
-				var body = doc.DocumentNode.SelectSingleNode("//body");
-				var images = body.Descendants("img")
-					.Where(e => !string.IsNullOrEmpty(e.GetAttributeValue("src", string.Empty)))
-					.ToList();
-
-				baseUri = (new UriBuilder(baseUri) { Host = $"onemore.{baseUri.Host}" }).Uri;
-
-				foreach (var image in images)
+				using (var source = new CancellationTokenSource(TimeSpan.FromSeconds(10)))
 				{
-					var src = image.GetAttributeValue("src", string.Empty);
-					if (!string.IsNullOrEmpty(src))
+					using (var response = await client.GetAsync(baseUri, source.Token))
 					{
-						src = new Uri(baseUri, src).AbsoluteUri;
-						var anchor = HtmlNode.CreateNode($"<a href=\"{src}\">{src}</a>");
-						image.ParentNode.ReplaceChild(anchor, image);
-					}
-				}
-
-				using (var one = new OneNote(out var page, out var ns))
-				{
-					// first update to add html to page
-
-					page.AddHtmlContent(doc.DocumentNode.OuterHtml);
-					await one.Update(page);
-
-					// fetch page again so we can transform anchors to downloaded images
-
-					page = one.GetPage(OneNote.PageDetail.All);
-
-					var regex = new Regex(@"<a\s+href=""[^:]+://(onemore\.)", RegexOptions.Compiled);
-
-					var runs = page.Root.DescendantNodes().OfType<XCData>()
-						.Where(c => regex.IsMatch(c.Value))
-						.Select(e => e.Parent)
-						.ToList();
-
-					// remove the "onemore." part of the host name from anchor URIs
-
-					foreach (var run in runs)
-					{
-						var matches = regex.Matches(run.Value);
-						if (matches.Count > 0)
+						if (response.IsSuccessStatusCode)
 						{
-							// handle multiple anchors in cdata, backtracking to avoid index overlaps
-							var text = run.Value;
-							for (int i = matches.Count - 1; i >= 0; i--)
-							{
-								text = text.Remove(
-									matches[i].Groups[1].Index, 
-									matches[i].Groups[1].Length);
-							}
-
-							run.Value = text;
+							content = await response.Content.ReadAsStringAsync();
 						}
-					}
-
-					// download and embed images
-					var cmd = new GetImagesCommand();
-					if (cmd.GetImages(page))
-					{
-						// second update to page
-						await one.Update(page);
 					}
 				}
 			}
+			catch (TaskCanceledException exc)
+			{
+				logger.WriteLine("timeout fetching web page", exc);
+				return;
+			}
+			catch (Exception exc)
+			{
+				logger.WriteLine("error fetching web page", exc);
+				return;
+			}
+
+			if (string.IsNullOrEmpty(content))
+			{
+				logger.WriteLine("web page return empty content");
+				return;
+			}
+
+			var doc = new HtmlDocument();
+			doc.LoadHtml(content);
+
+			// convert img tags to anchor tags
+			// OneNote will remove img tags but will keep anchors
+
+			var body = doc.DocumentNode.SelectSingleNode("//body");
+			var images = body.Descendants("img")
+				.Where(e => !string.IsNullOrEmpty(e.GetAttributeValue("src", string.Empty)))
+				.ToList();
+
+			baseUri = (new UriBuilder(baseUri) { Host = $"onemore.{baseUri.Host}" }).Uri;
+
+			foreach (var image in images)
+			{
+				var src = image.GetAttributeValue("src", string.Empty);
+				if (!string.IsNullOrEmpty(src))
+				{
+					src = new Uri(baseUri, src).AbsoluteUri;
+					var anchor = HtmlNode.CreateNode($"<a href=\"{src}\">{src}</a>");
+					image.ParentNode.ReplaceChild(anchor, image);
+				}
+			}
+
+			using (var one = new OneNote(out var page, out var ns))
+			{
+				// add html to page and let OneNote rehydrate as it sees fit
+				page.AddHtmlContent(doc.DocumentNode.OuterHtml);
+				await one.Update(page);
+				logger.WriteLine("pass 1 updated page with injected HTML");
+
+				try
+				{
+					// fetch page again with hydrated html
+					page = one.GetPage(OneNote.PageDetail.All);
+
+					// transform anchors to downloaded images..
+
+					var regex = new Regex(
+						@"<a\s+href=""[^:]+://(onemore\.)[^:]+://(onemore\.)",
+						RegexOptions.Compiled);
+
+					// download and embed images
+					var cmd = new GetImagesCommand(regex);
+					if (cmd.GetImages(page, ns))
+					{
+						// second update to page
+						await one.Update(page);
+						logger.WriteLine("pass 2 updated page with rehydrated images");
+					}
+				}
+				catch (Exception exc)
+				{
+					logger.WriteLine(exc);
+				}
+			}
+
+			logger.WriteTime("import web completed");
 		}
 	}
 }
