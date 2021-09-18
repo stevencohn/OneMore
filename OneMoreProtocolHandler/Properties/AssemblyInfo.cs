e98f794c92f76a30e9d037eb2721a15d044a70ef
Steven Cohn <stevencohn@live.com>
2021-09-18 13:00:22 -0400

    update protocol handler assemblyinfo

--- OneMoreProtocolHandler/Properties/AssemblyInfo.cs
+++ OneMoreProtocolHandler/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//************************************************************************************************
+// Copyright © 2021 Steven M Cohn. All rights reserved.
+//************************************************************************************************
+
+using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("OneMoreProtocolHandler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OneMore Add-in for OneNote Protocol Handler")]
+[assembly: AssemblyDescription("URL Protocol Handler")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("OneMoreProtocolHandler")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCompany("River Software")]
+[assembly: AssemblyProduct("OneMore")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2021 Steven M Cohn. All rights reserved.")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: CLSCompliant(true)]
 [assembly: Guid("bd0fa4fc-8ca9-4056-93dd-14537a758d48")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
