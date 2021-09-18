380cbe481b868557866fb28a25c6b49dcd18c200
Steven Cohn <stevencohn@live.com>
2021-09-17 21:26:34 -0400

    register/unregister

--- OneMoreProtocolHandler/Program.cs
+++ OneMoreProtocolHandler/Program.cs
@@ -2,12 +2,15 @@
 // Copyright © 2021 Steven M Cohn.  All rights reserved.
 //************************************************************************************************
 
+#pragma warning disable S1118 // Utility classes should not have public constructors
+
 namespace OneMoreProtocolHandler
 {
 	using Microsoft.Win32;
 	using System;
 	using System.Diagnostics;
 	using System.IO.Pipes;
+	using System.Reflection;
 	using System.Text;
 
 
@@ -18,14 +21,19 @@ namespace OneMoreProtocolHandler
 
 		static void Main(string[] args)
 		{
-			var arg = args[0];
-			if (arg == "--register")
+			if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+			{
+				// nothing to do
+				return;
+			}
+
+			if (args[0] == "--register")
 			{
 				Register();
 				return;
 			}
 
-			if (arg == "--unregister")
+			if (args[0] == "--unregister")
 			{
 				Unregister();
 				return;
@@ -74,15 +82,117 @@ namespace OneMoreProtocolHandler
 		}
 
 
+		// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
 		static void Register()
 		{
+			// protocol handler...
+			// Registers this program as the handler for the onemore:// protocol
 
+			/*
+			[HKEY_LOCAL_MACHINE\SOFTWARE\Classes\onemore]
+			@="URL:OneMore Protocol Handler"
+			"URL Protocol"=""
+
+			[HKEY_LOCAL_MACHINE\SOFTWARE\Classes\onemore\shell\open\command]
+			@="C:\\Github\\OneMore\\bin\\Debug\\OneMoreProtocolHandler.exe"
+			 */
+			using (var hive = RegistryKey.OpenBaseKey(
+				RegistryHive.LocalMachine, RegistryView.Registry64))
+			{
+				var path = @"Software\Classes\onemore";
+				var key = hive.OpenSubKey(path, true);
+				if (key == null)
+				{
+					key = hive.CreateSubKey(path, true);
+				}
+
+				key.SetValue(string.Empty, "URL:OneMore Protocol Handler");
+				key.SetValue("URL Protocol", string.Empty);
+				key.Dispose();
+
+				path = $@"{path}\shell\open\command";
+				key = hive.OpenSubKey(path, true);
+				if (key == null)
+				{
+					Debug.WriteLine($@"creating HKLM:\{path}");
+					key = hive.CreateSubKey(path, true);
+				}
+
+				key.SetValue(string.Empty, Assembly.GetExecutingAssembly().Location);
+				key.Dispose();
+			}
+
+			// trusted protocol...
+			// Declares the onemore: protocol as trusted so OneNote doesn't show a security dialog
+
+			/*
+			[HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Office\16.0\Common\Security\
+				Trusted Protocols\All Applications\onemore:]
+			*/
+			using (var hive = RegistryKey.OpenBaseKey(
+				RegistryHive.CurrentUser, RegistryView.Registry64))
+			{
+				var version = GetVersion("Excel", 16);
+				var path = $@"Software\Policies\Microsoft\Office\{version}\Common\Security\" +
+					@"Trusted Protocols\All Applications\onemore:";
+
+				var key = hive.OpenSubKey(path, true);
+				if (key == null)
+				{
+					Debug.WriteLine($@"creating HKCU:\{path}");
+					key = hive.CreateSubKey(path, true);
+				}
+
+				key.Dispose();
+			}
 		}
 
 
+		static Version GetVersion(string name, int latest)
+		{
+			using (var key = Registry.ClassesRoot.OpenSubKey($@"\{name}.Application\CurVer", false))
+			{
+				if (key != null)
+				{
+					// get default value string
+					var value = (string)key.GetValue(string.Empty);
+					// extract version number
+					var version = new Version(value.Substring(value.LastIndexOf('.') + 1) + ".0");
+					return version;
+				}
+			}
+
+			// presume latest
+			return new Version(latest, 0);
+		}
+
+
+		// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
 		static void Unregister()
 		{
+			// protocol handler...
+			using (var hive = RegistryKey.OpenBaseKey(
+				RegistryHive.LocalMachine, RegistryView.Registry64))
+			{
+				var path = @"Software\Classes\onemore";
+				Debug.WriteLine($@"deleting HKLM:\{path}");
+				hive.DeleteSubKeyTree(path, false);
+			}
 
+
+			// trusted protocol...
+			using (var hive = RegistryKey.OpenBaseKey(
+				RegistryHive.CurrentUser, RegistryView.Registry64))
+			{
+				var version = GetVersion("Excel", 16);
+				var path = $@"Software\Policies\Microsoft\Office\{version}\Common\Security\" +
+					@"Trusted Protocols\All Applications\onemore:";
+
+				Debug.WriteLine($@"deleting HKCU:\{path}");
+				hive.DeleteSubKey(path, false);
+			}
 		}
 	}
 }
