be7fcd43b4d62c8de47ee59236259cd3f98387f3
Steven Cohn <stevencohn@live.com>
2022-01-23 11:46:17 -0500

    load keyboard

--- OneMore/AddInHotkeys.cs
+++ OneMore/AddInHotkeys.cs
@@ -4,9 +4,11 @@
 
 namespace River.OneMoreAddIn
 {
+	using River.OneMoreAddIn.Settings;
 	using System;
 	using System.Linq;
-
+	using System.Windows.Forms;
+	using System.Xml.Linq;
 
 	public partial class AddIn
 	{
@@ -15,43 +17,46 @@ namespace River.OneMoreAddIn
 			var locale = System.Threading.Thread.CurrentThread.CurrentCulture.KeyboardLayoutId;
 			logger.WriteLine($"defining hotkeys for input locale {locale}");
 
+			var s = new SettingsProvider()
+				.GetCollection("KeyboardSheet")?.Get<XElement>("commands");
+
 			HotkeyManager.Initialize();
 
-			RegisterHotKey(nameof(AddFootnoteCmd), async () => await AddFootnoteCmd(null));
-			RegisterHotKey(nameof(AddFormulaCmd), async () => await AddFormulaCmd(null));
-			RegisterHotKey(nameof(ApplyStyle1Cmd), async () => await ApplyStyle1Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle2Cmd), async () => await ApplyStyle2Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle3Cmd), async () => await ApplyStyle3Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle4Cmd), async () => await ApplyStyle4Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle5Cmd), async () => await ApplyStyle5Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle6Cmd), async () => await ApplyStyle6Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle7Cmd), async () => await ApplyStyle7Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle8Cmd), async () => await ApplyStyle8Cmd(null));
-			RegisterHotKey(nameof(ApplyStyle9Cmd), async () => await ApplyStyle9Cmd(null));
-			RegisterHotKey(nameof(ClearLogCmd), async () => await ClearLogCmd(null));
-			RegisterHotKey(nameof(DecreaseFontSizeCmd), async () => await DecreaseFontSizeCmd(null));
-			RegisterHotKey(nameof(DiagnosticsCmd), async () => await DiagnosticsCmd(null));
-			RegisterHotKey(nameof(FillDownCmd), async () => await FillDownCmd(null));
-			RegisterHotKey(nameof(HighlightCmd), async () => await HighlightCmd(null));
-			RegisterHotKey(nameof(InsertCodeBlockCmd), async () => await InsertCodeBlockCmd(null));
-			RegisterHotKey(nameof(InsertDateCmd), async () => await InsertDateCmd(null));
-			RegisterHotKey(nameof(InsertDoubleHorizontalLineCmd), async () => await InsertDoubleHorizontalLineCmd(null));
-			RegisterHotKey(nameof(InsertHorizontalLineCmd), async () => await InsertHorizontalLineCmd(null));
-			RegisterHotKey(nameof(InsertTimerCmd), async () => await InsertTimerCmd(null));
-			RegisterHotKey(nameof(DisableSpellCheckCmd), async () => await DisableSpellCheckCmd(null));
-			RegisterHotKey(nameof(PasteRtfCmd), async () => await PasteRtfCmd(null));
-			RegisterHotKey(nameof(RecalculateFormulaCmd), async () => await RecalculateFormulaCmd(null));
-			RegisterHotKey(nameof(RemoveFootnoteCmd), async () => await RemoveFootnoteCmd(null));
-			RegisterHotKey(nameof(ReplayCmd), async () => await ReplayCmd(null));
-			RegisterHotKey(nameof(RemindCmd), async () => await RemindCmd(null));
-			RegisterHotKey(nameof(SearchCmd), async () => await SearchCmd(null));
-			RegisterHotKey(nameof(SearchAndReplaceCmd), async () => await SearchAndReplaceCmd(null));
-			RegisterHotKey(nameof(ShowXmlCmd), async () => await ShowXmlCmd(null));
-			RegisterHotKey(nameof(StartTimerCmd), async () => await StartTimerCmd(null));
-			RegisterHotKey(nameof(TaggedCmd), async () => await TaggedCmd(null));
-			RegisterHotKey(nameof(TaggingCmd), async () => await TaggingCmd(null));
-			RegisterHotKey(nameof(ToLowercaseCmd), async () => await ToLowercaseCmd(null));
-			RegisterHotKey(nameof(ToUppercaseCmd), async () => await ToUppercaseCmd(null));
+			RegisterHotKey(s, nameof(AddFootnoteCmd), async () => await AddFootnoteCmd(null));
+			RegisterHotKey(s, nameof(AddFormulaCmd), async () => await AddFormulaCmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle1Cmd), async () => await ApplyStyle1Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle2Cmd), async () => await ApplyStyle2Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle3Cmd), async () => await ApplyStyle3Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle4Cmd), async () => await ApplyStyle4Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle5Cmd), async () => await ApplyStyle5Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle6Cmd), async () => await ApplyStyle6Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle7Cmd), async () => await ApplyStyle7Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle8Cmd), async () => await ApplyStyle8Cmd(null));
+			RegisterHotKey(s, nameof(ApplyStyle9Cmd), async () => await ApplyStyle9Cmd(null));
+			RegisterHotKey(s, nameof(ClearLogCmd), async () => await ClearLogCmd(null));
+			RegisterHotKey(s, nameof(DecreaseFontSizeCmd), async () => await DecreaseFontSizeCmd(null));
+			RegisterHotKey(s, nameof(DiagnosticsCmd), async () => await DiagnosticsCmd(null));
+			RegisterHotKey(s, nameof(FillDownCmd), async () => await FillDownCmd(null));
+			RegisterHotKey(s, nameof(HighlightCmd), async () => await HighlightCmd(null));
+			RegisterHotKey(s, nameof(InsertCodeBlockCmd), async () => await InsertCodeBlockCmd(null));
+			RegisterHotKey(s, nameof(InsertDateCmd), async () => await InsertDateCmd(null));
+			RegisterHotKey(s, nameof(InsertDoubleHorizontalLineCmd), async () => await InsertDoubleHorizontalLineCmd(null));
+			RegisterHotKey(s, nameof(InsertHorizontalLineCmd), async () => await InsertHorizontalLineCmd(null));
+			RegisterHotKey(s, nameof(InsertTimerCmd), async () => await InsertTimerCmd(null));
+			RegisterHotKey(s, nameof(DisableSpellCheckCmd), async () => await DisableSpellCheckCmd(null));
+			RegisterHotKey(s, nameof(PasteRtfCmd), async () => await PasteRtfCmd(null));
+			RegisterHotKey(s, nameof(RecalculateFormulaCmd), async () => await RecalculateFormulaCmd(null));
+			RegisterHotKey(s, nameof(RemoveFootnoteCmd), async () => await RemoveFootnoteCmd(null));
+			RegisterHotKey(s, nameof(ReplayCmd), async () => await ReplayCmd(null));
+			RegisterHotKey(s, nameof(RemindCmd), async () => await RemindCmd(null));
+			RegisterHotKey(s, nameof(SearchCmd), async () => await SearchCmd(null));
+			RegisterHotKey(s, nameof(SearchAndReplaceCmd), async () => await SearchAndReplaceCmd(null));
+			RegisterHotKey(s, nameof(ShowXmlCmd), async () => await ShowXmlCmd(null));
+			RegisterHotKey(s, nameof(StartTimerCmd), async () => await StartTimerCmd(null));
+			RegisterHotKey(s, nameof(TaggedCmd), async () => await TaggedCmd(null));
+			RegisterHotKey(s, nameof(TaggingCmd), async () => await TaggingCmd(null));
+			RegisterHotKey(s, nameof(ToLowercaseCmd), async () => await ToLowercaseCmd(null));
+			RegisterHotKey(s, nameof(ToUppercaseCmd), async () => await ToUppercaseCmd(null));
 
 			// an awful hack to avoid a conflict with Italian keyboard (FIGS and likely UK) that
 			// use AltGr as Ctrl+Alt. This means users pressing AltGr+OemPlus to get a square
@@ -59,19 +64,43 @@ namespace River.OneMoreAddIn
 			// mean to! So here we only register these hot keys for the US keyboard input layout.
 			if (locale == 1033)
 			{
-				RegisterHotKey(nameof(IncreaseFontSizeCmd), async () => await IncreaseFontSizeCmd(null));
+				RegisterHotKey(s, nameof(IncreaseFontSizeCmd), async () => await IncreaseFontSizeCmd(null));
 			}
 		}
 
 
-		private void RegisterHotKey(string methodName, Action action)
+		private void RegisterHotKey(XElement settings, string methodName, Action action)
 		{			
 			if (typeof(AddIn).GetMethod(methodName)
 				.GetCustomAttributes(typeof(CommandAttribute), false)
 				.FirstOrDefault() is CommandAttribute cmd)
 			{
 				var hotkey = new Hotkey(cmd.DefaultKeys);
-				HotkeyManager.RegisterHotKey(action, hotkey);
+
+				var setting = settings?.Elements("command")
+					.FirstOrDefault(e => e.Attribute("command")?.Value == methodName);
+
+				if (setting != null)
+				{
+					if (Enum.TryParse<Keys>(setting.Attribute("keys")?.Value, true, out var keys))
+					{
+						// has user deliberately cleared command binding (?HasFlag doesn't work?)
+						if ((keys & Keys.KeyCode) == Keys.Back)
+						{
+							hotkey = null;
+						}
+						// has user overridden default binding
+						else if ((hotkey.Keys & hotkey.Modifiers) != keys)
+						{
+							hotkey = new Hotkey(keys);
+						}
+					}
+				}
+
+				if (hotkey != null)
+				{
+					HotkeyManager.RegisterHotKey(action, hotkey);
+				}
 			}
 		}
 	}
