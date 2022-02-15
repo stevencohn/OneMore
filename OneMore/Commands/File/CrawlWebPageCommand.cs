c13f61dfebdddd1afb6ef2e79e003643bd5e11a8
Steven Cohn <stevencohn@live.com>
2022-02-14 21:06:15 -0500

    crawl

--- OneMore/Commands/File/CrawlWebPageCommand.cs
+++ OneMore/Commands/File/CrawlWebPageCommand.cs
@@ -4,6 +4,8 @@
 
 namespace River.OneMoreAddIn.Commands
 {
+	using River.OneMoreAddIn.Models;
+	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text.RegularExpressions;
 	using System.Threading.Tasks;
@@ -11,6 +13,12 @@ namespace River.OneMoreAddIn.Commands
 
 	internal class CrawlWebPageCommand : Command
 	{
+		private sealed class Hyperlink
+		{
+			public string Address;
+			public string Text;
+		}
+
 		private OneNote one;
 
 
@@ -23,24 +31,44 @@ namespace River.OneMoreAddIn.Commands
 		{
 			using (one = new OneNote(out var page, out var ns, OneNote.PageDetail.Basic))
 			{
-				var links = page.Root.DescendantNodes().OfType<XCData>()
-					.Where(c => Regex.IsMatch(c.Value, $@"<a\s+href=""http[s]?://"));
-
-				foreach (var link in links)
+				var links = GetHyperlinks(page);
+				if (!links.Any())
 				{
-					var wrapper = link.GetWrapper();
-					var anchors = wrapper.Elements("a")
-						.Where(e => e.Attribute("href") != null &&
-							e.Attribute("href").Value.StartsWith("http"));
-
-					foreach (var anchor in anchors)
-					{
-						logger.WriteLine($"a {anchor.Attribute("href").Value} ({anchor.Value})");
-					}
+					return;
 				}
 			}
 
 			await Task.Yield();
+		}
+
+
+		private IEnumerable<Hyperlink> GetHyperlinks(Page page)
+		{
+			var links = new List<Hyperlink>();
+
+			var hrefs = page.Root.DescendantNodes().OfType<XCData>()
+				.Where(c => Regex.IsMatch(c.Value, $@"<a\s+href=""http[s]?://"));
+
+			foreach (var href in hrefs)
+			{
+				var wrapper = href.GetWrapper();
+				var anchors = wrapper.Elements("a")
+					.Where(e => e.Attribute("href") != null &&
+						e.Attribute("href").Value.StartsWith("http"));
+
+				foreach (var anchor in anchors)
+				{
+					logger.WriteLine($"a {anchor.Attribute("href").Value} ({anchor.Value})");
+
+					links.Add(new Hyperlink
+					{
+						Address = anchor.Attribute("href").Value,
+						Text = anchor.Value
+					});
+				}
+			}
+
+			return links;
 		}
 	}
 }
