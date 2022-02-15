33885c772d6f621d660432bdd68edcad28dbd7c9
Steven Cohn <stevencohn@live.com>
2022-02-14 19:36:17 -0500

    crawl poc

--- OneMore/Commands/File/CrawlWebPageCommand.cs
+++ OneMore/Commands/File/CrawlWebPageCommand.cs
@@ -4,15 +4,16 @@
 
 namespace River.OneMoreAddIn.Commands
 {
-	using System;
-	using System.Collections.Generic;
 	using System.Linq;
-	using System.Text;
+	using System.Text.RegularExpressions;
 	using System.Threading.Tasks;
-
+	using System.Xml.Linq;
 
 	internal class CrawlWebPageCommand : Command
 	{
+		private OneNote one;
+
+
 		public CrawlWebPageCommand()
 		{
 		}
@@ -20,6 +21,25 @@ namespace River.OneMoreAddIn.Commands
 
 		public override async Task Execute(params object[] args)
 		{
+			using (one = new OneNote(out var page, out var ns, OneNote.PageDetail.Basic))
+			{
+				var links = page.Root.DescendantNodes().OfType<XCData>()
+					.Where(c => Regex.IsMatch(c.Value, $@"<a\s+href=""http[s]?://"));
+
+				foreach (var link in links)
+				{
+					var wrapper = link.GetWrapper();
+					var anchors = wrapper.Elements("a")
+						.Where(e => e.Attribute("href") != null &&
+							e.Attribute("href").Value.StartsWith("http"));
+
+					foreach (var anchor in anchors)
+					{
+						logger.WriteLine($"a {anchor.Attribute("href").Value} ({anchor.Value})");
+					}
+				}
+			}
+
 			await Task.Yield();
 		}
 	}
