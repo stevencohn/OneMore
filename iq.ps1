0d051864eae85434effb4eeaaf5de384de729e88
Steven Cohn <stevencohn@live.com>
2022-02-19 11:00:26 -0500

    IQ

--- iq.ps1
+++ iq.ps1
@@ -61,99 +61,115 @@ Begin
     function CheckAppID
     {
 	    $0 = "Registry::HKEY_CLASSES_ROOT\AppID\$guid"
-        if (-not (HasKey $0)) { return }
-        if (-not (HasValue $0 'DllSurrogate' '')) { return }
-        Write-Host "OK $0"
+        $ok = (HasKey $0)
+        if ($ok) { $ok = (HasValue $0 'DllSurrogate' '') }
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
     }
 
     function CheckRoot
     {
 	    $0 = "Registry::HKEY_CLASSES_ROOT\onemore"
-        if (-not (HasKey $0)) { return }
-        if (-not (HasValue $0 '(default)' 'URL:OneMore Protocol Handler')) { return }
-        if (-not (HasValue $0 'URL Protocol' '')) { return }
-        Write-Host "OK $0"
+        $ok = (HasKey $0)
+        if ($ok) {
+            $ok = (HasValue $0 '(default)' 'URL:OneMore Protocol Handler') -and $ok
+            $ok = (HasValue $0 'URL Protocol' '') -and $ok
+        }
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
     }
 
     function CheckShell
     {
 	    $0 = "Registry::HKEY_CLASSES_ROOT\onemore\shell\open\command"
-        if (-not (HasKey $0)) { return }
-        if (-not (HasValue $0 '(default)' '*\OneMoreProtocolHandler.exe %1 %2 %3 %4 %5')) { return }
-        Write-Host "OK $0"
+        $ok = (HasKey $0)
+        if ($ok) { $ok = (HasValue $0 '(default)' '*\OneMoreProtocolHandler.exe %1 %2 %3 %4 %5') }
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
     }
 
     function CheckAddIn
     {
 	    $0 = "Registry::HKEY_CLASSES_ROOT\River.OneMoreAddIn"
-        if (-not (HasKey $0)) { return }
-        if (-not (HasValue $0 '(default)' 'River.OneMoreAddIn.AddIn')) { return }
-	    $1 = "$0\CLSID"
-        if (-not (HasValue $1 '(default)' $guid)) { return }
-	    $1 = "$0\CurVer"
-        if (-not (HasValue $1 '(default)' 'River.OneMoreAddIn.1')) { return }
-        Write-Host "OK $0"
+        $ok = (HasKey $0)
+        if ($ok) {
+            $ok = (HasValue $0 '(default)' 'River.OneMoreAddIn.AddIn') -and $ok
+	        $1 = "$0\CLSID"
+            $ok = (HasValue $1 '(default)' $guid) -and $ok
+	        $1 = "$0\CurVer"
+            $ok = (HasValue $1 '(default)' 'River.OneMoreAddIn.1') -and $ok
+        }
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
 
 	    $0 = "Registry::HKEY_CLASSES_ROOT\River.OneMoreAddIn.1"
-        if (-not (HasValue $0 '(default)' 'Addin class')) { return }
+        $ok = (HasValue $0 '(default)' 'Addin class')
 	    $1 = "$0\CLSID"
-        if (-not (HasValue $1 '(default)' $guid)) { return }
-        Write-Host "OK $0"
+        $ok = (HasValue $1 '(default)' $guid) -and $ok
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
     }
 
     function CheckCLSID
     {
 	    $0 = "Registry::HKEY_CLASSES_ROOT\CLSID\$guid"
-        if (-not (HasKey $0)) { return }
-        if (-not (HasValue $0 '(default)' 'River.OneMoreAddIn.AddIn')) { return }
-        if (-not (HasValue $0 'AppID' $guid)) { return }
+        $ok = (HasKey $0)
+        if ($ok) {
+            $ok = (HasValue $0 '(default)' 'River.OneMoreAddIn.AddIn')
+            $ok = (HasValue $0 'AppID' $guid) -and $ok
+        }
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
 
         $1 = "$0\Implemented Categories\{62C8FE65-4EBB-45E7-B440-6E39B2CDBF29}"
-        if (-not (HasKey $0)) { return }
-
-        $1 = "$0\InprocServer32"
-        if (-not (HasKey $1)) { return }
-        if (-not (HasValue $1 '(default)' 'mscoree.dll')) { return }
-        if (-not (HasValue $1 'ThreadingModel' 'Both')) { return }
-        if (-not (HasValue $1 'CodeBase' '*\River.OneMoreAddIn.dll')) { return }
-        if (-not (HasValue $1 'Class' 'River.OneMoreAddIn.AddIn')) { return }
-        if (-not (HasValue $1 'RuntimeVersion' 'v*')) { return }
-        if (-not (HasValue $1 'Assembly' 'River.OneMoreAddIn, Version=*')) { return }
+        $ok = (HasKey $0)
+        if ($ok) {
+            $1 = "$0\InprocServer32"
+            $ok = (HasKey $1)
+            if ($ok) {
+                $ok = (HasValue $1 '(default)' 'mscoree.dll')
+                $ok = (HasValue $1 'ThreadingModel' 'Both') -and $ok
+                $ok = (HasValue $1 'CodeBase' '*\River.OneMoreAddIn.dll') -and $ok
+                $ok = (HasValue $1 'Class' 'River.OneMoreAddIn.AddIn') -and $ok
+                $ok = (HasValue $1 'RuntimeVersion' 'v*') -and $ok
+                $ok = (HasValue $1 'Assembly' 'River.OneMoreAddIn, Version=*') -and $ok
+            }
+        }
+        if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
 
         $1 = "$0\InprocServer32\"
 
         $1 = "$0\ProgID"
-        if (-not (HasKey $1)) { return }
-        if (-not (HasValue $1 '(default)' 'River.OneMoreAddIn')) { return }
+        $ok = (HasKey $1)
+        if ($ok) { $ok = (HasValue $1 '(default)' 'River.OneMoreAddIn') }
+        if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
 
         $1 = "$0\Programmable"
-        if (-not (HasKey $1)) { return }
-        if (-not (HasValue $1 '(default)' '')) { return }
+        $ok = (HasKey $1)
+        if ($ok) { $ok = (HasValue $1 '(default)' '') }
+        if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
 
         $1 = "$0\TypeLib"
-        if (-not (HasKey $1)) { return }
-        if (-not (HasValue $1 '(default)' $guid)) { return }
+        $ok = (HasKey $1)
+        if ($ok) { $ok = (HasValue $1 '(default)' $guid) }
+        if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
 
         $1 = "$0\VersionIndependentProgID"
-        if (-not (HasKey $1)) { return }
-        if (-not (HasValue $1 '(default)' 'River.OneMoreAddIn')) { return }
-
-        Write-Host "OK $0"
+        $ok = (HasKey $1)
+        if ($ok) { $ok = (HasValue $1 '(default)' 'River.OneMoreAddIn') }
+        if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
     }
 
     function CheckUser
     {
         $0 = "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\AppID\$guid"
-        if (-not (HasKey $0)) { return }
-        if (-not (HasValue $0 'DllSurrogate' '')) { return }
+        $ok = (HasKey $0)
+        if ($ok) { $ok = (HasValue $0 'DllSurrogate' '') }
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
 
         $0 = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Office\OneNote\AddIns\River.OneMoreAddIn"
-        if (-not (HasValue $0 'LoadBehavior' '3')) { return }
-        if (-not (HasValue $0 'Description' 'Extension for OneNote')) { return }
-        if (-not (HasValue $0 'FriendlyName' 'OneMoreAddIn')) { return }
+        $ok = (HasValue $0 'LoadBehavior' '3')
+        $ok = (HasValue $0 'Description' 'Extension for OneNote') -and $ok
+        $ok = (HasValue $0 'FriendlyName' 'OneMoreAddIn') -and $ok
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
 
         $0 = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\River.OneMoreAddIn.dll"
-        if (-not (HasValue $0 'Path' '*\River.OneMoreAddIn.dll')) { return }
+        $ok = (HasValue $0 'Path' '*\River.OneMoreAddIn.dll')
+        if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
     }
 }
 Process
