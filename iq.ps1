8067e9784f06fb23b5bfea9892a39011355ed018
Steven Cohn <stevencohn@live.com>
2022-02-20 07:31:55 -0500

    iq

--- iq.ps1
+++ iq.ps1
@@ -36,23 +36,28 @@ Begin
 
     function HasValue
     {
-        param($kpath, $name, $value)
+        param($kpath, $name, $value, [switch] $match)
         if (-not (HasProperty $kpath $name))
         {
             return $false
         }
-		$v = (Get-ItemPropertyValue -Path $kpath -Name $name)
+		$script:lastValue = (Get-ItemPropertyValue -Path $kpath -Name $name)
         if ($value.Contains('*'))
         {
-            if (-not ($v -like $value))
+            if (-not ($lastValue -like $value))
             {
-                Write-Host "invalid value: $kpath\$name, '$v' <> '$value'" -Fore Red
+                Write-Host "invalid value: $kpath\$name, '$lastValue' <> '$value'" -Fore Red
                 return $false
             }
         }
-        elseif ($v -ne $value)
+        elseif ($lastValue -ne $value)
         {
-            Write-Host "invalid value: $kpath\$name, '$v' <> '$value'" -Fore Red
+            if ($match) {
+                Write-Host "mismatch $kpath\$name, '$lastValue' <> '$value'" -Fore Yellow
+            }
+            else {
+                Write-Host "invalid value: $kpath\$name, '$lastValue' <> '$value'" -Fore Red
+            }
             return $false
         }
         return $true
@@ -116,6 +121,7 @@ Begin
         if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
 
         $1 = "$0\Implemented Categories\{62C8FE65-4EBB-45E7-B440-6E39B2CDBF29}"
+        $ver = $null
         $ok = (HasKey $0)
         if ($ok) {
             $1 = "$0\InprocServer32"
@@ -123,15 +129,46 @@ Begin
             if ($ok) {
                 $ok = (HasValue $1 '(default)' 'mscoree.dll')
                 $ok = (HasValue $1 'ThreadingModel' 'Both') -and $ok
+                
                 $ok = (HasValue $1 'CodeBase' '*\River.OneMoreAddIn.dll') -and $ok
-                $ok = (HasValue $1 'Class' 'River.OneMoreAddIn.AddIn') -and $ok
-                $ok = (HasValue $1 'RuntimeVersion' 'v*') -and $ok
-                $ok = (HasValue $1 'Assembly' 'River.OneMoreAddIn, Version=*') -and $ok
+                if ($ok) { $script:codeBase = $lastValue }
+
+                $oo = (HasValue $1 'Class' 'River.OneMoreAddIn.AddIn')
+                if ($oo) { $script:class = $lastValue }
+                $ok = $oo -and $ok
+
+                $oo = (HasValue $1 'RuntimeVersion' 'v*')
+                if ($oo) { $script:runtimeVersion = $lastValue }
+                $ok = $oo -and $ok
+
+                $oo = (HasValue $1 'Assembly' 'River.OneMoreAddIn, Version=*')
+                if ($oo) { $script:assembly = $lastValue }
+                $ok = $oo -and $ok
+
+                if ($oo)
+                {
+                    if ($assembly -match ',\sVersion=([0-9\.]+),\s')
+                    {
+                        $ver = $matches[1]
+                    }
+                }
             }
         }
         if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
 
-        $1 = "$0\InprocServer32\"
+        if ($ver)
+        {
+            $1 = "$0\InprocServer32\$ver"
+            $ok = (HasValue $1 'Assembly' $assembly -match)
+            $ok = (HasValue $1 'CodeBase' $codeBase -match) -and $ok
+            $ok = (HasValue $1 'RuntimeVersion' $runtimeVersion -match) -and $ok
+            $ok = (HasValue $1 'Class' $class -match) -and $ok
+            if ($ok) { Write-Host "OK $1" } else { Write-Host "BAD $1" }
+        }
+        else
+        {
+            Write-Host "skipping $0\InprocServer32\<version>" -Fore Yellow
+        }
 
         $1 = "$0\ProgID"
         $ok = (HasKey $1)
@@ -168,7 +205,7 @@ Begin
         if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
 
         $0 = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\River.OneMoreAddIn.dll"
-        $ok = (HasValue $0 'Path' '*\River.OneMoreAddIn.dll')
+        $ok = (HasValue $0 'Path' $codeBase -match)
         if ($ok) { Write-Host "OK $0" } else { Write-Host "BAD $0" }
     }
 }
