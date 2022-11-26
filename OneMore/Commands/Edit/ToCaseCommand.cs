e38613de7610360b54e0db6f936254888afdaa11
Steven Cohn <stevencohn@live.com>
2022-11-26 13:17:39 -0500

    use APA basic recommendations for title casing

--- OneMore/Commands/Edit/ToCaseCommand.cs
+++ OneMore/Commands/Edit/ToCaseCommand.cs
@@ -4,7 +4,9 @@
 
 namespace River.OneMoreAddIn.Commands
 {
+	using System;
 	using System.Globalization;
+	using System.Text.RegularExpressions;
 	using System.Threading;
 	using System.Threading.Tasks;
 	using System.Xml.Linq;
@@ -16,12 +18,12 @@ namespace River.OneMoreAddIn.Commands
 		public const int Uppercase = 1;
 		public const int Titlecase = 3;
 
-		private readonly TextInfo info;
+		private const string pattern =
+			@"(?:[""']|[^:;,./<>?!%\)])\s+(and|an|as|at|but|by|for|if|in|nor|of|off|or|per|so|to|up|via|yet)\W";
 
 
 		public ToCaseCommand ()
 		{
-			info = Thread.CurrentThread.CurrentCulture.TextInfo;
 		}
 
 
@@ -52,17 +54,60 @@ namespace River.OneMoreAddIn.Commands
 
 		private string Recase(string text, int casing)
 		{
+			var info = Thread.CurrentThread.CurrentCulture;
+
 			if (casing == Lowercase)
 			{
-				return info.ToLower(text);
+				return info.TextInfo.ToLower(text);
 			}
 
 			if (casing == Uppercase)
 			{
-				return info.ToUpper(text);
+				return info.TextInfo.ToUpper(text);
 			}
 
-			return info.ToTitleCase(text);
+			if (info.TwoLetterISOLanguageName == "en")
+			{
+				return ToProperTitleCase(info, text);
+			}
+
+			return info.TextInfo.ToTitleCase(text);
 		}
+
+
+		/*
+		In title case, capitalize the following words in a title or heading
+		- the first word of the title or heading, even if it is a minor word [The | A]
+		- the first word of a subtitle
+		- the first word after a colon, em dash, or end punctuation in a heading
+		- major words including the 2nd part of hyphenated major words [Self-Report, not Self-report]
+		- words of four letters or more [With | Between | From]
+
+		Lowercase only minor words that are three letters or fewer in a title or heading except
+		the first word in a title or subtitle or the first word after a colon, em dash, or end
+		punctuation in a heading
+		- short conjunctions [and | as | but | for | if | nor | or | so | yet]
+		- articles [a | an | the]
+		- short prepositions [as | at | by | for | in | of | off | on | per | to | up | via]
+		 */
+		private string ToProperTitleCase(CultureInfo info, string text)
+		{
+			// capitalize each word
+			text = info.TextInfo.ToTitleCase(text.ToLower(info));
+
+			// lowercase the exceptions...
+
+			var chars = text.ToCharArray();
+			var matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+
+			foreach (Match match in matches)
+			{
+				var index = match.Groups[1].Index;
+				chars[index] = Char.ToLower(chars[index]);
+			}
+
+			return new string(chars);
+		}
+
 	}
 }
