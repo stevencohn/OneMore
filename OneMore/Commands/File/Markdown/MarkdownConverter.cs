f898c15639a199f2f7195cbd049a9583698e4d2d
Steven Cohn <stevencohn@live.com>
2022-11-27 10:18:04 -0500

    apply quick style defs to candidate headings

--- OneMore/Commands/File/Markdown/MarkdownConverter.cs
+++ OneMore/Commands/File/Markdown/MarkdownConverter.cs
@@ -9,13 +9,14 @@ namespace River.OneMoreAddIn.Commands
 	using System.Linq;
 	using System.Xml.Linq;
 
+
 	internal class MarkdownConverter
 	{
 		private sealed class Candidate
 		{
 			public XElement Element;
 			public Style Style;
-			public StandardStyles? Standard;
+			public StandardStyles? Key;
 		}
 
 
@@ -24,28 +25,32 @@ namespace River.OneMoreAddIn.Commands
 			var analyzer = new StyleAnalyzer(page.Root);
 			var ns = page.Namespace;
 
-			var headings = page.Root.Elements(ns + "Outline")
+			page.Root.Elements(ns + "Outline")
 				.Descendants(ns + "OE")
+				// candidate headings imported from markdown should have exactly one text run
 				.Where(e => e.Elements(ns + "T").Count() == 1)
 				.Select(e => new Candidate
 				{
 					Element = e,
-					Style = analyzer.CollectStyleFrom(e.Elements(ns + "T").First())
+					// deep dive into style of text run
+					Style = new Style(analyzer.CollectFrom(e.Elements(ns + "T").First(), true))
 				})
 				.Select(c =>
 				{
-					c.Standard = MatchHeading(c.Style);
+					// have to match heading after Style is set in previous Select
+					c.Key = MatchHeading(c.Style);
 					return c;
 				})
-				.Where(c => c.Standard != null);
-
-			headings
-				.Where(heading => heading.Standard != null)
+				// shouldn't happen but...
+				.Where(c => c.Key != null)
 				.ForEach(heading =>
 				{
-					var quick = page.GetQuickStyle((StandardStyles)heading.Standard);
+					// ensures quick style is declared if not already
+					var quick = page.GetQuickStyle((StandardStyles)heading.Key);
 
+					// nix inline style we added during phase 1 import
 					heading.Element.Attributes().Where(a => a.Name == "style").Remove();
+					// set heading quick style on OE
 					heading.Element.SetAttributeValue("quickStyleIndex", quick.Index);
 
 					var stylizer = new Stylizer(quick);
@@ -54,6 +59,7 @@ namespace River.OneMoreAddIn.Commands
 						.Elements(ns + "T")
 						.ForEach(e =>
 						{
+							// set any additional css on text run such as italics
 							stylizer.ApplyStyle(e);
 						});
 				});
@@ -65,22 +71,24 @@ namespace River.OneMoreAddIn.Commands
 			if (style.FontFamily == "Calibri")
 			{
 				if (style.FontSize == "16.0" && style.Color == "#1E4E79")
+				{
 					return StandardStyles.Heading1;
+				}
 
 				if (style.FontSize == "14.0" && style.Color == "#2E75B5")
+				{
 					return StandardStyles.Heading2;
+				}
 
 				if (style.FontSize == "12.0" && style.Color == "#5B9BD5")
-					return StandardStyles.Heading3;
-
-				if (style.FontSize == "12.0" && style.Color == "#5B9BD5" && style.IsItalic)
-					return StandardStyles.Heading4;
+				{
+					return style.IsItalic ? StandardStyles.Heading4 : StandardStyles.Heading3;
+				}
 
 				if (style.FontSize == "11.0" && style.Color == "#2E75B5")
-					return StandardStyles.Heading5;
-
-				if (style.FontSize == "11.0" && style.Color == "#2E75B5" && style.IsItalic)
-					return StandardStyles.Heading6;
+				{
+					return style.IsItalic ? StandardStyles.Heading6 : StandardStyles.Heading5;
+				}
 			}
 
 			return null;
