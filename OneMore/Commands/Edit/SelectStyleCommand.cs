c9b31c969c01de908b5b651a816d135955e1b54f
Steven Cohn <stevencohn@live.com>
2022-11-30 12:27:33 -0500

    edit command comments

--- OneMore/Commands/Edit/SelectStyleCommand.cs
+++ OneMore/Commands/Edit/SelectStyleCommand.cs
@@ -10,7 +10,7 @@ namespace River.OneMoreAddIn.Commands
 	using System.Linq;
 	using System.Threading.Tasks;
 	using System.Xml.Linq;
-	using Resx = River.OneMoreAddIn.Properties.Resources;
+	using Resx = Properties.Resources;
 
 
 	/// <summary>
