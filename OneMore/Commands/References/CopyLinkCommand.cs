b6c10c842b6118d0c2d893946cd564196a9e85fd
Steven Cohn <stevencohn@live.com>
2022-12-02 12:35:51 -0500

    comments

--- OneMore/Commands/References/CopyLinkCommand.cs
+++ OneMore/Commands/References/CopyLinkCommand.cs
@@ -32,85 +32,86 @@ namespace River.OneMoreAddIn
 			string hyperlink = null;
 			string text = null;
 
-			using (var one = new OneNote(out var page, out var ns))
+			using var one = new OneNote(out var page, out var ns);
+
+			if (specific)
 			{
-				if (specific)
+				var selected = page.Root.Elements(ns + "Outline")
+					.Where(e => !e.Elements(ns + "Meta")
+						.Any(m => m.Attribute("name").Value.Equals(MetaNames.TaggingBank)))
+					.Descendants(ns + "OE")
+					.LastOrDefault(e => e.Attributes().Any(a => a.Name == "selected"));
+
+				if (selected != null)
 				{
-					var selected = page.Root.Elements(ns + "Outline")
-						.Where(e => !e.Elements(ns + "Meta")
-							.Any(m => m.Attribute("name").Value.Equals(MetaNames.TaggingBank)))
-						.Descendants(ns + "OE")
-						.LastOrDefault(e => e.Attributes().Any(a => a.Name == "selected"));
-
-					if (selected != null)
+					if (selected.GetAttributeValue("objectID", out var objectID))
 					{
-						if (selected.GetAttributeValue("objectID", out var objectID))
+						hyperlink = one.GetHyperlink(page.PageId, objectID);
+						if (!string.IsNullOrEmpty(hyperlink))
 						{
-							hyperlink = one.GetHyperlink(page.PageId, objectID);
-							if (!string.IsNullOrEmpty(hyperlink))
-							{
-								// remove any descendant Images to prevent bad XML parsing
-								// from brokets in OCR text, also remove indented paragraphs
-								selected.Descendants(ns + "Image").Remove();
-								selected.Descendants(ns + "OEChildren").Remove();
+							// remove any descendant Images to prevent bad XML parsing
+							// from brokets in OCR text, also remove indented paragraphs
+							selected.Descendants(ns + "Image").Remove();
+							selected.Descendants(ns + "OEChildren").Remove();
 
-								text = selected.TextValue();
-								if (text.Length > 20)
-								{
-									text = $"{text.Substring(0, 20)}...";
-								}
+							text = selected.TextValue();
+							if (text.Length > 20)
+							{
+								text = $"{text.Substring(0, 20)}...";
 							}
 						}
-					}
-					else
-					{
-						UIHelper.ShowError(Resx.Error_BodyContext);
 					}
 				}
 				else
 				{
-					hyperlink = one.GetHyperlink(page.PageId, null);
-				}
-
-				if (!string.IsNullOrEmpty(hyperlink))
-				{
-					// build from hierarchy to avoid splitting on '/' where there is a slash
-					// in any part of the name
-
-					var crumbs = new StringBuilder();
-					var id = one.GetParent(page.PageId);
-					while (!string.IsNullOrEmpty(id))
-					{
-						var node = one.GetHierarchyNode(id);
-
-						// following line could be used to hyperlink each part like a breadcrumb
-						//crumbs.Insert(0, $"<a href={node.Link}>{node.Name}</a> {RightArrow} ");
-
-						crumbs.Insert(0, $"{node.Name} {RightArrow} ");
-						id = one.GetParent(id);
-					}
-
-					crumbs.Append(page.Title);
-					var path = specific ? $"{crumbs} {RightArrow} <i>{text}</i>" : crumbs.ToString();
-
-					var builder = new StringBuilder();
-					builder.AppendLine("<html>");
-					builder.AppendLine("<body>");
-					builder.AppendLine("<!--StartFragment-->");
-					builder.AppendLine($"<a href='{hyperlink}'>{path}</a>");
-					builder.AppendLine("<!--EndFragment-->");
-					builder.AppendLine("</body>");
-					builder.AppendLine("</html>");
-					var html = PasteRtfCommand.AddHtmlPreamble(builder.ToString());
-
-					// copy hyperlink to clipboard
-					await new ClipboardProvider().SetHtml(html);
-
-					MoreBubbleWindow.Show(specific
-						? Resx.CopyLinkCommand_LinkToParagraph
-						: Resx.CopyLinkCommand_LinkToPage);
+					UIHelper.ShowError(Resx.Error_BodyContext);
 				}
 			}
+			else
+			{
+				hyperlink = one.GetHyperlink(page.PageId, null);
+			}
+
+			if (string.IsNullOrEmpty(hyperlink))
+			{
+				return;
+			}
+
+			// build from hierarchy to avoid splitting on '/' where there is a slash
+			// in any part of the name
+
+			var crumbs = new StringBuilder();
+			var id = one.GetParent(page.PageId);
+			while (!string.IsNullOrEmpty(id))
+			{
+				var node = one.GetHierarchyNode(id);
+
+				// following line could be used to hyperlink each part like a breadcrumb
+				//crumbs.Insert(0, $"<a href={node.Link}>{node.Name}</a> {RightArrow} ");
+
+				crumbs.Insert(0, $"{node.Name} {RightArrow} ");
+				id = one.GetParent(id);
+			}
+
+			crumbs.Append(page.Title);
+			var path = specific ? $"{crumbs} {RightArrow} <i>{text}</i>" : crumbs.ToString();
+
+			var builder = new StringBuilder();
+			builder.AppendLine("<html>");
+			builder.AppendLine("<body>");
+			builder.AppendLine("<!--StartFragment-->");
+			builder.AppendLine($"<a href='{hyperlink}'>{path}</a>");
+			builder.AppendLine("<!--EndFragment-->");
+			builder.AppendLine("</body>");
+			builder.AppendLine("</html>");
+			var html = PasteRtfCommand.AddHtmlPreamble(builder.ToString());
+
+			// copy hyperlink to clipboard
+			await new ClipboardProvider().SetHtml(html);
+
+			MoreBubbleWindow.Show(specific
+				? Resx.CopyLinkCommand_LinkToParagraph
+				: Resx.CopyLinkCommand_LinkToPage);
 
 			await Task.Yield();
 		}
