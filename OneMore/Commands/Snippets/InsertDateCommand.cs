6e2103109f40be707f2b4e5350c87bb807f1aec2
Steven Cohn <stevencohn@live.com>
2024-04-29 10:24:04 -0400

    Refine InsertDateCommand to avoid null exceptions #1344

--- OneMore/Commands/Snippets/InsertDateCommand.cs
+++ OneMore/Commands/Snippets/InsertDateCommand.cs
@@ -1,5 +1,5 @@
 ﻿//************************************************************************************************
-// Copyright © 2020 Steven M Cohn.  All rights reserved.
+// Copyright © 2020 Steven M Cohn. All rights reserved.
 //************************************************************************************************
 
 namespace River.OneMoreAddIn.Commands
@@ -19,47 +19,57 @@ namespace River.OneMoreAddIn.Commands
 
 		public override async Task Execute(params object[] args)
 		{
-			var includeTime = (bool)args[0];
-
 			await using var one = new OneNote(out var page, out var ns);
-			var elements = page.Root.Descendants(ns + "T")
-				.Where(e => e.Attribute("selected")?.Value == "all");
 
+			var includeTime = (bool)args[0];
 			var text = DateTime.Now.ToString(includeTime ? "yyy-MM-dd hh:mm tt" : "yyy-MM-dd");
-
 			var content = new XElement(ns + "T", new XCData(text));
 
-			if (elements == null)
+			var cursor = page.GetTextCursor(allowPageTitle: true);
+
+			if (page.SelectionScope == SelectionScope.Region || page.SelectionSpecial)
 			{
-				// empty page so add new content
-				page.AddNextParagraph(content);
-			}
-			else if (elements.Count() > 1)
-			{
-				// selected multiple runs so replace them all
+				// replace region or hyperlink/MathML
 				page.ReplaceSelectedWithContent(content);
+			}
+			else if (cursor == null) // && page.SelectionScope == SelectionScope.Empty)
+			{
+				// can't find cursor to append to page
+				page.AddNextParagraph(content);
 			}
 			else
 			{
-				var line = elements.First();
-				if (line.Parent.Parent.Name.LocalName == "Title" &&
-					line.Value == line.Parent.Value)
+				var line = page.Root .Descendants(ns + "T")
+					.FirstOrDefault(e => 
+						e.Attributes().Any(a => a.Name == "selected" && a.Value == "all"));
+
+				if (line is null)
 				{
-					// special case to insert date before heading;
-					// if cursor is before first char of title, the entire title is "selected"
-					// so rather than replace the title, just insert the date before it
-					var cdata = line.GetCData();
-					cdata.Value = $"{text} {cdata.Value}";
-				}
-				else if (line.Value.Length == 0)
-				{
-					// empty cdata, unselected cursor so just insert
-					line.GetCData().Value = text;
+					// this case should not happen; should be handled above
+					page.AddNextParagraph(content);
 				}
 				else
 				{
-					// something is selected so replace it
-					page.ReplaceSelectedWithContent(content);
+					if (line.FirstAncestor(ns + "Title", ns + "Outline") is XElement title)
+					{
+						// special case to insert date before heading;
+						// if cursor is before first char of title, the entire title is "selected"
+						// so rather than replace the title, just insert the date before it
+						var first = title.Elements(ns + "OE").Elements(ns + "T").First();
+						var cdata = first.GetCData();
+						cdata.Value = $"{text} {cdata.Value}";
+					}
+					else if (line.Value.Length == 0)
+					{
+						// empty cdata, unselected cursor so just insert
+						line.GetCData().Value = text;
+					}
+					else
+					{
+						// this case should not happen; should be handled above
+						// something is selected so replace it
+						page.ReplaceSelectedWithContent(content);
+					}
 				}
 			}
 
