96625cb40a2e95f746bfd36c51380ae00dd8e4a5
Steven Cohn <stevencohn@live.com>
2024-05-15 11:19:20 -0400

    convert #1028

--- OneMore/Commands/Edit/ConvertMarkdownCommand.cs
+++ OneMore/Commands/Edit/ConvertMarkdownCommand.cs
@@ -9,7 +9,6 @@ namespace River.OneMoreAddIn.Commands
 	using System.IO;
 	using System.Linq;
 	using System.Text;
-	using System.Text.RegularExpressions;
 	using System.Threading.Tasks;
 	using System.Xml.Linq;
 
