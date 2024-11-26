bbf2f64a113a2d98887700d77153ca62996201aa
Matthias Weiss <matthias.h.weiss@arcor.de>
2024-11-26 08:26:14 +0100

    added selective UseEmojiAndSmiley to allow seperate tag handling

--- OneMore/Commands/File/Markdown/OneMoreDigExtensions.cs
+++ OneMore/Commands/File/Markdown/OneMoreDigExtensions.cs
@@ -5,13 +5,33 @@
 namespace River.OneMoreAddIn.Commands
 {
 	using Markdig;
-
+	using Markdig.Extensions.Emoji;
+	using System.Collections.Generic;
+	using System.Linq;
+	using River.OneMoreAddIn.Helpers.Office;
+	using System.Web.UI.WebControls;
 
 	internal static class OneMoreDigExtensions
 	{
+
 		public static MarkdownPipelineBuilder UseOneMoreExtensions(
 			this MarkdownPipelineBuilder pipeline)
 		{
+			var emojiDic = EmojiMapping.GetDefaultEmojiShortcodeToUnicode();
+			var emojiDicNew = new Dictionary<string, string>();
+			foreach (var mappings in emojiDic)
+			{
+				var tagName = Models.Page.taglist.FirstOrDefault(x => x.name.Equals(mappings.Key)).name;
+				if (tagName.IsNullOrEmpty())
+				{
+					emojiDicNew.Add(mappings.Key,mappings.Value);
+				}
+			}
+			var DefaultEmojisAndSmileysMapping = new EmojiMapping(
+								emojiDicNew, EmojiMapping.GetDefaultSmileyToEmojiShortcode());
+			//			var emojiMapping = EmojiMapping.DefaultEmojisAndSmileysMapping;
+			pipeline.Extensions.Add(new EmojiExtension(DefaultEmojisAndSmileysMapping));
+
 			pipeline.Extensions.Add(new OneMoreDigExtension());
 			return pipeline;
 		}
