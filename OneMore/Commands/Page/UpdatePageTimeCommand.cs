755ce20d3450c6249c35c82179f6794869674c4c
Steven <stevencohn@users.noreply.github.com>
2024-12-16 14:24:10 -0500

    Add update page time command (#1704)
    #1702

---
+++ OneMore/Commands/Page/UpdatePageTimeCommand.cs
@@ -0,0 +1,25 @@
+﻿//************************************************************************************************
+// Copyright © 2024 Steven M Cohn. All rights reserved.
+//************************************************************************************************
+
+namespace River.OneMoreAddIn.Commands
+{
+	using System;
+	using System.Threading.Tasks;
+
+
+	internal class UpdatePageTimeCommand : Command
+	{
+		public UpdatePageTimeCommand()
+		{
+		}
+
+
+		public override async Task Execute(params object[] args)
+		{
+			await using var one = new OneNote(out var page, out _, OneNote.PageDetail.Basic);
+			page.Root.SetAttributeValue("dateTime", DateTime.Now.ToZuluString());
+			await one.Update(page);
+		}
+	}
+}
