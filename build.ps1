4fc1246d003d3b77b20063c6c8e9f2956008b899
Steven <stevencohn@users.noreply.github.com>
2025-07-27 18:32:35 -0400

    1769 arm64 (#1852)
    * arm test
    #1769
    
    * update build script
    
    * update build script
    
    * update configs
    
    * update architecture checks in code
    
    * updates
    
    * Set default ribbon to separate tab
    
    * update fast build
    
    * rethinking
    
    * remove pa
    
    * update
    
    * checksum
    
    * fini
    
    * fini2
    
    * fini
    
    * webview2loader
    
    * sqlite.interop
    
    * fini
    
    * fini

--- build.ps1
+++ build.ps1
@@ -1,36 +1,42 @@
 <#
 .SYNOPSIS
-Build both x86 and x64 msi
+Build OneMore full installer kit for the specified architecture, or default project builds.
 
-.PARAMETER ConfigBits
-Specifies the bitness of the build: 64 or 86, default is 64
+.PARAMETER Architecture
+Builds the installer kit for the specifies architecture: x86 (default), x64, ARM64, or All.
 
-.PARAMETER Both
-Build both x86 and x64 kits.
+.PARAMETER Clean
+Clean all projects in the solution, removing all bin and obj directories.
+No build is performed.
 
 .PARAMETER Fast
-Build just the .csproj projects using default parameters.
-This will build OneMore, OneMorCalendar, OneMoreProtocolHandler, and OneMoreSetupActions.
+Build just the .csproj projects using default parameters:
+OneMore, OneMorCalendar, OneMoreProtocolHandler, OneMoreSetupActions, and OneMoreTray.
 
 .PARAMETER Prep
 Run DisableOutOfProcBuild. This only needs to be run once on a machine, or after upgrading
 or reinstalling Visual Studio. It is required to build installer kits from the command line.
+No build is performed.
+
+.PARAMETER VLog
+Enable verbose logging for MSBuild. This is useful for debugging build issues.
 #>
 
-# CmdletBinding adds -Verbose functionality, SupportsShouldProcess adds -WhatIf
 [CmdletBinding(SupportsShouldProcess = $true)]
 param (
-	[int] $configbits = 64,
-	[switch] $fast,
-	[switch] $both,
-	[switch] $prep
+	[ValidateSet('x86','x64','ARM64','All')]
+	[string] $Architecture = 'x86',
+	[string] $Detect,
+	[switch] $Clean,
+	[switch] $Fast,
+	[switch] $Prep,
+	[switch] $VLog
 	)
 
 Begin
 {
-	$script:devenv = ''
-	$script:vsregedit = ''
-	$script:vdproj = ''
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	# Helpers...
 
 	function FindVisualStudio
 	{
@@ -40,6 +46,7 @@ Begin
 			$script:devenv = $cmd.Source
 			$script:ideroot = Split-Path -Parent $devenv
 			$script:vsregedit = Join-Path $ideroot 'VSRegEdit.exe'
+			write-Verbose "... devenv found at $devenv"
 			return $true
 		}
 
@@ -53,29 +60,65 @@ Begin
 	function FindVS
 	{
 		param($vsroot)
-		$script:devenv = Join-Path $vsroot 'Enterprise\Common7\IDE\devenv.com'
-
+		$script:devenv = Join-Path $vsroot 'Professional\Common7\IDE\devenv.com'
 		if (!(Test-Path $devenv))
 		{
-			$script:devenv = Join-Path $vsroot 'Professional\Common7\IDE\devenv.com'
-		}
-		if (!(Test-Path $devenv))
-		{
-			$script:devenv = Join-Path $vsroot 'Community\Common7\IDE\devenv.com'
-		}
-
-		if (!(Test-Path $devenv))
-		{
-			Write-Host "devenv not found in $vsroot" -ForegroundColor Yellow
-			return $false
+			$script:devenv = Join-Path $vsroot 'Enterprise\Common7\IDE\devenv.com'
+			if (!(Test-Path $devenv))
+			{
+				$script:devenv = Join-Path $vsroot 'Community\Common7\IDE\devenv.com'
+				if (!(Test-Path $devenv))
+				{
+					Write-Host "devenv not found in $vsroot" -ForegroundColor Yellow
+					return $false
+				}
+			}
 		}
 
 		$script:ideroot = Split-Path -Parent $devenv
 		$script:vsregedit = Join-Path $ideroot 'VSRegEdit.exe'
+		write-Verbose "... devenv found at $devenv"
 		return $true
 	}
 
-	function DisableOutOfProcBuild
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	# Single Commands...
+
+	function CleanSolution
+	{
+		$pushpop = (!(Test-Path OneMore.sln))
+		if ($pushpop) { Push-Location .. }
+		CleanProject 'OneMore'
+		CleanProject 'OneMoreCalendar'
+		CleanProject 'OneMoreProtocolHandler'
+		CleanProject 'OneMoreSetup'
+		CleanProject 'OneMoreSetupActions'
+		CleanProject 'OneMoreTray'
+		if ($pushpop) { Pop-Location }
+	}
+	
+	function CleanProject
+	{
+		param([string]$project)
+		Push-Location $project
+		try
+		{
+			Write-verbose "... cleaning $project"
+			$progpref = $ProgressPreference
+			$ProgressPreference = 'SilentlyContinue' 
+			if (Test-Path bin) { Remove-Item bin -Recurse -Force -Confirm:$false | Out-Null }
+			if (Test-Path obj) { Remove-Item obj -Recurse -Force -Confirm:$false | Out-Null }
+			if (Test-Path Debug) { Remove-Item Debug -Recurse -Force -Confirm:$false | Out-Null }
+			if (Test-Path Release) { Remove-Item Release -Recurse -Force -Confirm:$false | Out-Null }
+			$ProgressPreference = $progpref
+		}
+		finally
+		{
+			Pop-Location
+		}
+	}
+
+	function DisablPrepOutOfProcBuild
 	{
 		$0 = Join-Path $ideroot 'CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild'
 		if (Test-Path $0)
@@ -91,81 +134,330 @@ Begin
 		Write-Host "*** could not find $0\DisableOutOfProcBuild.exe" -ForegroundColor Yellow
 	}
 
+	function DetectArchitecture
+	{
+		param($dllpath)
+
+		$dllpath = resolve-Path $dllpath
+		if (-not (Test-Path $DllPath)) {
+			Write-Error "File not found: $DllPath"
+			return
+		}
+
+		# Open the file as a binary stream
+		$stream = [System.IO.File]::OpenRead($DllPath)
+		try {
+			$reader = New-Object System.Reflection.PortableExecutable.PEReader $stream
+			$machine = $reader.PEHeaders.CoffHeader.Machine
+
+			switch ([int]$machine) {
+				0x014c { "x86" }
+				0x0200 { "Itanium" }
+				0x8664 { "x64" }
+				0x01c4 { "ARM" }
+				0xaa64 { "ARM64" }
+				default { "Unknown Architecture: $machine" }
+			}
+		} finally {
+			$stream.Close()
+		}
+	}
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	# Fast...
+
+	function BuildFast
+	{
+		Write-Host "... fast build with default configs" -ForegroundColor Yellow
+
+		NugetRestore 'OneMore'
+		BuildProject 'OneMore'
+
+		NugetRestore 'OneMoreTray'
+		BuildProject 'OneMoreTray'
+
+		NugetRestore 'OneMoreCalendar'
+		BuildProject 'OneMoreCalendar'
+
+		BuildProject 'OneMoreProtocolHandler'
+
+		BuildProject 'OneMoreSetupActions'
+
+		ReportArchitectures
+	}
+
+	function NugetRestore
+	{
+		param($name)
+		Push-Location $name
+
+		$cmd = "nuget restore .\$name.csproj"
+		write-Host $cmd -ForegroundColor DarkGray
+		nuget restore .\$name.csproj -solutiondirectory ..
+
+		Pop-Location
+	}
+
+	function BuildProject
+	{
+		param($name)
+		Push-Location $name
+
+		# output file cannot exist before build
+		if (Test-Path .\Debug\*)
+		{
+			Remove-Item .\Debug\*.* -Force -Confirm:$false
+		}
+
+		$cmd = "$devenv .\$name.csproj /project $name /projectconfig 'Debug|AnyCPU' /build"
+		write-Host $cmd -ForegroundColor DarkGray
+		. $devenv .\$name.csproj /project $name /projectconfig 'Debug|AnyCPU' /build
+
+		Pop-Location
+	}
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	# Kit...
+
+	function Build
+	{
+		param($arc)
+		$script:Architecture = $arc
+
+		CleanSolution
+		RestoreSolution
+
+		if (BuildSolution)
+		{
+			ReportArchitectures
+			BuildKit
+		}
+	}
+
+	function RestoreSolution
+	{
+		Write-Host "`n... restoring nuget packages" -ForegroundColor Cyan
+		Write-Host
+
+		NugetRestore 'OneMore'
+		NugetRestore 'OneMoreTray'
+		NugetRestore 'OneMoreCalendar'
+	}
+
+	function BuildSolution
+	{
+		Write-Host "`n... building $Architecture solution" -ForegroundColor Cyan
+		Write-Host
+
+		SetBuildVerbosity 4
+
+		try
+		{
+			$log = "$($env:TEMP)\OneMoreBuild.log"
+			if (Test-Path $log) { Remove-Item $log -Force -Confirm:$false }
+
+			$cmd = "$devenv .\OneMore.sln /build 'Debug|$Architecture' /out '$log'"
+			Write-Host $cmd -ForegroundColor DarkGray
+			. $devenv .\OneMore.sln /build "Debug|$Architecture" /out $log
+		}
+		finally
+		{
+			SetBuildVerbosity 1
+		}
+
+		$succeeded = 0;
+		$failed = 1
+
+		Get-Content $env:TEMP\OneMoreBuild.log -ErrorAction SilentlyContinue | `
+			where { $_ -match '== Build: (\d+) succeeded, (\d+) failed'} | `
+			select -last 1 | `
+			foreach {
+				$succeeded = $matches[1]
+				$failed = $matches[2]
+				$color = $failed -eq 0 ? 'Green' : 'Red'
+				write-Host "`n... build completed: $succeeded succeeded, $failed failed" -ForegroundColor $color
+			}
+
+		return [bool]($succeeded -gt 0 -and $failed -eq 0)
+	}
+
+	function SetBuildVerbosity
+	{
+		param($level)
+		if ($VLog)
+		{
+			$desc = $level -eq 4 ? 'enabling' : 'disabling'
+			Write-Host "... $desc MSBuild verbose logging" -ForegroundColor DarkYellow
+			$cmd = ". '$vsregedit' set local HKCU General MSBuildLoggerVerbosity dword $level`n"
+			write-Host $cmd -ForegroundColor DarkGray
+			. $vsregedit set local HKCU General MSBuildLoggerVerbosity dword $level | Out-Null
+		}
+	}
+
+	function ReportArchitectures
+	{
+		$arc = (DetectArchitecture .\OneMore\bin\$Architecture\Debug\River.OneMoreAddIn.dll)
+		Write-Host "... OneMore: $arc" -ForegroundColor DarkGray
+
+		ReportModuleArchitecture 'OneMoreCalendar'
+		ReportModuleArchitecture 'OneMoreProtocolHandler'
+		ReportModuleArchitecture 'OneMoreSetupActions'
+		ReportModuleArchitecture 'OneMoreTray'
+		Write-Host
+	}
+
+	function ReportModuleArchitecture
+	{
+		param($name)
+		$arc = (DetectArchitecture .\$name\bin\Debug\$name.exe)
+		Write-Host "... $name`: $arc" -ForegroundColor DarkGray
+	}
+
+	function BuildKit
+	{
+		Write-Host "`n... building $Architecture kit" -ForegroundColor Cyan
+		Write-Host
+
+		Push-Location OneMoreSetup
+		$vdproj = Resolve-Path .\OneMoreSetup.vdproj
+
+		PreserveVdproj $vdproj
+
+		try
+		{
+			ConfigureSetupProject $vdproj
+
+			$log = "$($env:TEMP)\OneMoreBuild.log"
+			$cmd = "$devenv .\OneMoreSetup.vdproj /build 'Debug|$Architecture' /project Setup /out '$log'"
+			write-Host $cmd -ForegroundColor DarkGray
+
+			. $devenv .\OneMoreSetup.vdproj /build "Debug|$Architecture" /project Setup /out $env:TEMP\OneMorebuild.log
+
+			if ($LASTEXITCODE -eq 0)
+			{
+				$0 = Get-ChildItem .\Debug\OneMore_*.msi | select -first 1
+				if (Test-Path $0)
+				{
+					# move msi to Downloads for safe-keeping and to allow next Platform build
+					$1 = "$home\Downloads\OneMore_$productVersion`_Setup$Architecture.msi"
+					Move-Item $0 $1 -Force -Confirm:$false
+					Write-Host "... $Architecture MSI copied to $1" -ForegroundColor DarkYellow
+
+					if (Get-Command checksum -ErrorAction SilentlyContinue)
+					{
+						if (Test-Path $0)
+						{
+							$sum = (checksum -t sha256 $0)
+							Write-Host "... $Architecture checksum: $sum" -ForegroundColor DarkYellow
+						}
+					}
+				}
+			}
+		}
+		finally
+		{
+			RestoreVdproj $vdproj
+			Pop-Location
+		}
+	}
+
 	function PreserveVdproj
 	{
+		param($vdproj)
 		Write-Host '... preserving vdproj' -ForegroundColor DarkGray
+
+		Write-Verbose '... restoring vdproj from git'
+		git restore $vdproj
+
 		Copy-Item $vdproj .\vdproj.tmp -Force -Confirm:$false
 	}
 
 	function RestoreVdproj
 	{
+		param($vdproj)
 		Write-Host '... restoring vdproj' -ForegroundColor DarkGray
-		Copy-Item .\vdproj.tmp $vdproj -Force -Confirm:$false
-		Remove-Item .\vdproj.tmp -Force
+		$0 = (Resolve-Path .\vdproj.tmp)
+		if (Test-Path $0)
+		{
+			Copy-Item $0 $vdproj -Force -Confirm:$false
+			Remove-Item $0 -Force -Confirm:$false
+		}
 	}
 
-	function Configure
+	function ConfigureSetupProject
 	{
-		param([int]$bitness)
+		param($vdproj)
 		$lines = @(Get-Content $vdproj)
 
 		$script:productVersion = $lines | `
-			Where-Object { $_ -match '"ProductVersion" = "8:(.+?)"' } | `
-			ForEach-Object { $matches[1] }
+			where { $_ -match '"ProductVersion" = "8:(.+?)"' } | `
+			foreach { $matches[1] }
 
 		Write-Host
-		Write-Host "... configuring vdproj for x$bitness build of $productVersion" -ForegroundColor Yellow
+		Write-Host "... configuring vdproj for $Architecture build of $productVersion" -ForegroundColor Yellow
 
 		'' | Out-File $vdproj -nonewline
 
-		$lines | ForEach-Object `
+		$lines | foreach `
 		{
 			if ($_ -match '"OutputFileName" = "')
 			{
-				# "OutputFilename" = "8:Debug\\OneMore_v_Setupx86.msi"
+				# "OutputFilename" = "8:Debug\\OneMore_v_Setupx64.msi"
 				$line = $_.Replace('OneMore_v_', "OneMore_$($productVersion)_")
-				if ($bitness -eq 64) {
-					$line.Replace('x86', 'x64') | Out-File $vdproj -Append
-				} else {
-					$line.Replace('x64', 'x86') | Out-File $vdproj -Append
-				}
+				$line.Replace('x64', $Architecture) | Out-File $vdproj -Append
 			}
 			elseif ($_ -match '"DefaultLocation" = "')
 			{
 				# "DefaultLocation" = "8:[ProgramFilesFolder][Manufacturer]\\[ProductName]"
-				if ($bitness -eq 64) {
-					$_.Replace('ProgramFilesFolder', 'ProgramFiles64Folder') | Out-File $vdproj -Append
-				} else {
+				if ($Architecture -ge 'x86') {
 					$_.Replace('ProgramFiles64Folder', 'ProgramFilesFolder') | Out-File $vdproj -Append
+				} else {
+					$_.Replace('ProgramFilesFolder', 'ProgramFiles64Folder') | Out-File $vdproj -Append
 				}
 			}
 			elseif ($_ -match '"TargetPlatform" = "')
 			{
 				# x86 -> "3:0"
 				# x64 -> "3:1"
-				if ($bitness -eq 64) {
-					'"TargetPlatform" = "3:1"' | Out-File $vdproj -Append
-				} else {
+				if ($Architecture -ge 'x86') {
 					'"TargetPlatform" = "3:0"' | Out-File $vdproj -Append
+				} else {
+					'"TargetPlatform" = "3:1"' | Out-File $vdproj -Append
 				}
 			}
-			#elseif ($_ -match '"SourcePath" = .*WebView2Loader\.dll"$')
-			#{
-			#    if ($bitness -eq 64) {
-			#        $_.Replace('\\x86', '\\x64') | Out-File $vdproj -Append
-			#        $_.Replace('win-x86', 'win-x64') | Out-File $vdproj -Append
-			#    } else {
-			#        $_.Replace('\\x64', '\\x86') | Out-File $vdproj -Append
-			#        $_.Replace('win-x64', 'win-x86') | Out-File $vdproj -Append
-			#    }
-			#}
 			elseif (($_ -match '"Name" = "8:OneMoreSetupActions --install ') -or `
 					($_ -match '"Arguments" = "8:--install '))
 			{
-				if ($bitness -eq 64) {
+				# "Name" = "8:OneMoreSetupActions --install --x86"
+				# "Arguments" = "8:--install --x86"
+				$_.Replace('x86', $Architecture) | Out-File $vdproj -Append
+			}
+			elseif ($_ -match '"SourcePath" = .*WebView2Loader\.dll"$')
+			{
+				if ($Architecture -ne 'x86')
+				{
+					$line = $_.Replace('x86', $Architecture)
+					$line.Replace('x64', $Architecture) | Out-File $vdproj -Append
+				}
+				else
+				{
+					$_ | Out-File $vdproj -Append
+				}
+			}
+			elseif ($_ -match '"SourcePath" = .*SQLite.Interop\.dll"$')
+			{
+				if ($Architecture -eq 'x64')
+				{
 					$_.Replace('x86', 'x64') | Out-File $vdproj -Append
-				} else {
-					$_.Replace('x64', 'x86') | Out-File $vdproj -Append
+				}
+				elseif ($Architecture -eq 'ARM64')
+				{
+					$line = $_.Replace('bin\\x86', 'bin\\ARM64')
+					$line.Replace('Debug\\x86', 'Debug\\x64') | Out-File $vdproj -Append
+				}
+				else
+				{
+					$_ | Out-File $vdproj -Append
 				}
 			}
 			elseif ($_ -notmatch '^"Scc')
@@ -174,147 +466,35 @@ Begin
 			}
 		}
 	}
-
-	function BuildFast
-	{
-		param([int]$bitness)
-		Write-Host "... building x$bitness DLLs" -ForegroundColor Yellow
-
-		# build...
-
-		Push-Location OneMore
-		BuildComponent 'OneMore' $true
-		Pop-Location
-
-		Push-Location OneMoreTray
-		BuildComponent 'OneMoreTray' $true
-		Pop-Location
-
-		Push-Location OneMoreCalendar
-		BuildComponent 'OneMoreCalendar' $true
-		Pop-Location
-
-		Push-Location OneMoreProtocolHandler
-		BuildComponent 'OneMoreProtocolHandler'
-		Pop-Location
- 
-		Push-Location OneMoreSetupActions
-		BuildComponent 'OneMoreSetupActions'
-		Pop-Location
-	}
-
-	function BuildComponent
-	{
-		param($name, $restore = $false)
-		# output file cannot exist before build
-		if (Test-Path .\Debug\*)
-		{
-			Remove-Item .\Debug\*.* -Force -Confirm:$false
-		}
-		# nuget restore
-		if ($restore)
-		{
-			$cmd = 'nuget restore .\$name.csproj'
-			write-Host $cmd -ForegroundColor DarkGray
-			nuget restore .\$name.csproj
-		}
-		$cmd = "$devenv .\$name.csproj /build ""Debug|AnyCPU"" /project $name /projectconfig Debug"
-		write-Host $cmd -ForegroundColor DarkGray
-		. $devenv .\$name.csproj /build "Debug|AnyCPU" /project $name /projectconfig Debug
-	}
-
-	function Build
-	{
-		param([int]$bitness)
-		Write-Host "... building x$bitness MSI" -ForegroundColor Yellow
-
-		# output file cannot exist before build
-		if (Test-Path .\Debug\*)
-		{
-			Remove-Item .\Debug\*.* -Force -Confirm:$false
-		}
-
-		if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent)
-		{
-			Write-Host '... enabling MSBuild verbose logging' -ForegroundColor DarkYellow
-			$cmd = ". '$vsregedit' set local HKCU General MSBuildLoggerVerbosity dword 4`n"
-			write-Host $cmd -ForegroundColor DarkGray
-			. $vsregedit set local HKCU General MSBuildLoggerVerbosity dword 4 | Out-Null
-		}
-
-		$cmd = "$devenv .\OneMoreSetup.vdproj /build ""Debug|x$bitness"" /project Setup /projectconfig Debug /out `$env:TEMP\OneMoreBuild.log"
-		write-Host $cmd -ForegroundColor DarkGray
-
-		# build
-		. $devenv .\OneMoreSetup.vdproj /build "Debug|x$bitness" /project Setup /projectconfig Debug /out $env:TEMP\OneMorebuild.log
-
-		# move msi to Downloads for safe-keeping and to allow next Platform build
-		Move-Item .\Debug\*.msi $home\Downloads -Force
-		Write-Host "... x$bitness MSI copied to $home\Downloads\" -ForegroundColor DarkYellow
-
-		if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent)
-		{
-			Write-Host '... disabling MSBuild verbose logging' -ForegroundColor DarkYellow
-			$cmd = ". '$vsregedit' set local HKCU General MSBuildLoggerVerbosity dword 1`n"
-			write-Host $cmd -ForegroundColor DarkGray
-			. $vsregedit set local HKCU General MSBuildLoggerVerbosity dword 1 | Out-Null
-		}
-	}
 }
 Process
 {
-	if (-not (FindVisualStudio))
-	{
-		return
-	}
-	
-	if ($prep)
-	{
-		DisableOutOfProcBuild
-		return
-	}
+	$script:verboseColor = $PSStyle.Formatting.Verbose
+	$PSStyle.Formatting.Verbose = $PSStyle.Foreground.BrightBlack
 
-	if ($fast)
+	if (-not (FindVisualStudio)) { return }
+
+	if ($Detect) { DetectArchitecture $Detect; return }
+
+	if ($Prep) { DisablPrepOutOfProcBuild; return }
+	if ($Clean) { CleanSolution; return }
+
+	if ($Fast) { BuildFast; return }
+
+	if ($Architecture -eq 'All')
 	{
-		BuildFast 64
+		Build 'ARM64'
+		Build 'x64'
+		Build 'x86'
 	}
 	else
 	{
-		Push-Location OneMoreSetup
-		$script:vdproj = Resolve-Path .\OneMoreSetup.vdproj
-		
-		git restore $vdproj
+		if ($Architecture -eq 'arm64') { $Architecture = 'ARM64' }
 
-		PreserveVdproj
-
-		$check = Get-Command checksum -ErrorAction SilentlyContinue
-
-		if ($configbits -eq 86 -or $both)
-		{
-			Configure 86
-			Build 86
-
-			if ($check)
-			{
-				$sum = (checksum -t sha256 C:\Users\steve\Downloads\OneMore_$($productVersion)_Setupx86.msi)
-				Write-Host "... x86 checksum: $sum" -ForegroundColor DarkYellow
-			}
-		}
-
-		if ($configBits -eq 64 -or $both)
-		{
-			Configure 64
-			Build 64
-
-			if ($check)
-			{
-				$sum = (checksum -t sha256 C:\Users\steve\Downloads\OneMore_$($productVersion)_Setupx64.msi)
-				Write-Host "... x64 checksum: $sum" -ForegroundColor DarkYellow
-			}
-		}
-
-		RestoreVdproj
+		Build $Architecture
 	}
-
-	Pop-Location
+}
+End
+{
+	$PSStyle.Formatting.Verbose = $verboseColor
 }
