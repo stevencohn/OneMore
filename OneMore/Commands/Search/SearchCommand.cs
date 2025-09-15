a841e14dbd281ecad9d418497bb058fb87d098bb
Steven Cohn <stevencohn@live.com>
2025-09-15 06:47:01 -0400

    stub

---
+++ OneMore/Commands/Search/SearchCommand.cs
@@ -0,0 +1,35 @@
+﻿//************************************************************************************************
+// Copyright © 2025 Steven M Cohn. All rights reserved.
+//************************************************************************************************
+
+namespace River.OneMoreAddIn.Commands
+{
+	using System.Linq;
+	using System.Threading.Tasks;
+	using System.Xml.Linq;
+
+
+	internal class SearchCommand : Command
+	{
+		public SearchCommand()
+		{
+		}
+
+
+		public override async Task Execute(params object[] args)
+		{
+			await using var one = new OneNote(out var page, out var ns);
+
+			var paragraphs = page.BodyOutlines
+				.Descendants(ns + "OE")
+				.Where(e => e.Elements(ns + "T").Any());
+
+			if (paragraphs.Any())
+			{
+				foreach (var paragraph in paragraphs)
+				{
+				}
+			}
+		}
+	}
+}
