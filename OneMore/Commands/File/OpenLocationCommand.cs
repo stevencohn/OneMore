318acb3dbe5a8a15aba0d92e3fb5c2df7f2de9c6
stevencohn <stevencohn@live.com>
2026-04-25 07:59:20 -0400

    Allow Open File Location to open OneDrive hosted sections #1998

--- OneMore/Commands/File/OpenLocationCommand.cs
+++ OneMore/Commands/File/OpenLocationCommand.cs
@@ -4,14 +4,25 @@
 
 namespace River.OneMoreAddIn.Commands
 {
+	using System;
+	using System.Diagnostics;
 	using System.IO;
 	using System.Linq;
 	using System.Threading.Tasks;
 	using Resx = Properties.Resources;
 
 
+	/// <summary>
+	/// Open the currently selected section or section group in the file explorer or browser, 
+	/// depending on the path type.
+	/// </summary>
 	internal class OpenLocationCommand : Command
 	{
+		private const string SectionName = "Section";
+		private const string SectionGroupName = "SectionGroup";
+		private const string isCurrentlyViewedAtt = "isCurrentlyViewed";
+
+
 		public OpenLocationCommand()
 		{
 		}
@@ -23,44 +34,54 @@ namespace River.OneMoreAddIn.Commands
 			var notebook = await one.GetNotebook();
 			var ns = one.GetNamespace(notebook);
 
-			var node = notebook.Descendants(ns + "Section")
+			var node = notebook.Descendants(ns + SectionName)
 				.LastOrDefault(e =>
-					e.Attribute("isCurrentlyViewed") is not null &&
-					e.Attribute("isCurrentlyViewed").Value == "true");
+					e.Attribute(isCurrentlyViewedAtt) is not null &&
+					e.Attribute(isCurrentlyViewedAtt).Value == "true");
 
 			if (node is null)
 			{
-				node = notebook.Descendants(ns + "SectionGroup")
+				node = notebook.Descendants(ns + SectionGroupName)
 					.LastOrDefault(e =>
-						e.Attribute("isCurrentlyViewed") is not null &&
-						e.Attribute("isCurrentlyViewed").Value == "true");
+						e.Attribute(isCurrentlyViewedAtt) is not null &&
+						e.Attribute(isCurrentlyViewedAtt).Value == "true");
 			}
 
-			if (node is null)
+			if (node is not null)
 			{
-				UI.MoreMessageBox.ShowWarning(owner, Resx.OpenLocationCommand_NoPath);
-				return;
+				var path = node.Attribute("path")?.Value;
+				if (path is not null)
+				{
+					if (
+						// check if well-formed URL
+						Uri.TryCreate(path, UriKind.Absolute, out var uri) &&
+						uri.Scheme == Uri.UriSchemeHttps)
+					{
+						Process.Start(new ProcessStartInfo
+						{
+							FileName = path,
+							UseShellExecute = true
+						});
+						return;
+					}
+
+					if (path.EndsWith(".one"))
+					{
+						// we only need the directory path
+						path = Path.GetDirectoryName(path);
+					}
+
+					if (
+						// check if local file path
+						Path.IsPathRooted(path) && Directory.Exists(path))
+					{
+						Process.Start("explorer.exe", path);
+						return;
+					}
+				}
 			}
 
-			var path = node.Attribute("path")?.Value;
-			if (path is null)
-			{
-				UI.MoreMessageBox.ShowWarning(owner, Resx.OpenLocationCommand_NoPath);
-				return;
-			}
-
-			if (path.EndsWith(".one"))
-			{
-				path = Path.GetDirectoryName(path);
-			}
-
-			if (!Path.IsPathRooted(path) || !Directory.Exists(path))
-			{
-				UI.MoreMessageBox.ShowWarning(owner, Resx.OpenLocationCommand_NoPath);
-				return;
-			}
-
-			System.Diagnostics.Process.Start("explorer.exe", path);
+			UI.MoreMessageBox.ShowWarning(owner, Resx.OpenLocationCommand_NoPath);
 		}
 	}
 }
