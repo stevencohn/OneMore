9df5172ee4fb05fad0a2e18f3a8f750f30f03032
stevencohn <stevencohn@live.com>
2026-05-21 19:51:24 -0400

    pixels

--- OneMore/Commands/Search/SearchDialogTextControl.Designer.cs
+++ OneMore/Commands/Search/SearchDialogTextControl.Designer.cs
@@ -171,7 +171,7 @@ namespace River.OneMoreAddIn.Commands
 			this.searchButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
 			this.searchButton.Image = global::River.OneMoreAddIn.Properties.Resources.m_Search;
 			this.searchButton.ImageOver = null;
-			this.searchButton.Location = new System.Drawing.Point(706, 11);
+			this.searchButton.Location = new System.Drawing.Point(706, 9);
 			this.searchButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 15);
 			this.searchButton.Name = "searchButton";
 			this.searchButton.ShowBorder = true;
