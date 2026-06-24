62cd6e9491cf452aeeee8d72ae71872ea5b18aec
Steven <stevencohn@users.noreply.github.com>
2026-06-24 09:18:18 -0400

    2268 remove hashtag scanner gc collection (#2269)
    Relates to #2268

--- OneMore/Commands/Tagging/HashtagPageScannerFactory.cs
+++ OneMore/Commands/Tagging/HashtagPageScannerFactory.cs
@@ -5,6 +5,7 @@
 namespace River.OneMoreAddIn.Commands
 {
 	using Models;
+	using System.Collections.Concurrent;
 	using System.Text.RegularExpressions;
 	using System.Xml.Linq;
 
@@ -15,6 +16,12 @@ namespace River.OneMoreAddIn.Commands
 	/// </summary>
 	internal class HashtagPageSannerFactory
 	{
+		// RegexOptions.Compiled JITs via Reflection.Emit into a dynamic assembly that is
+		// never unloaded on .NET Framework, so the compiled pattern is cached for the life
+		// of the process instead of being recompiled on every scan (every 2 minutes)
+		private static readonly ConcurrentDictionary<(bool Unfiltered, bool Doubled), Regex>
+			patternCache = new();
+
 		private readonly Regex hashPattern;
 		private readonly XElement styleTemplate;
 
@@ -24,6 +31,13 @@ namespace River.OneMoreAddIn.Commands
 		/// </summary>
 		public HashtagPageSannerFactory(XElement styleTemplate, bool unfiltered, bool doubled)
 		{
+			hashPattern = patternCache.GetOrAdd((unfiltered, doubled), key => BuildPattern(key.Unfiltered, key.Doubled));
+			this.styleTemplate = styleTemplate;
+		}
+
+
+		private static Regex BuildPattern(bool unfiltered, bool doubled)
+		{
 			// these force only ##hashtags and exclude all #hashtags
 			var pounds = doubled ? "##" : "#";
 			var count = doubled ? "2,2" : "1,2";
@@ -31,13 +45,11 @@ namespace River.OneMoreAddIn.Commands
 			// TODO: right-to-left languages?
 			// Groups[1].Index, Length, Value
 			// matches ##digits or ##word or #word, but not #digits
-			hashPattern = new Regex(unfiltered
+			return new Regex(unfiltered
 				? @$"(?:^|[\s\[\({{,])(##\d[\w\-_]{{0,}}|{pounds}{{1,2}}[^\W\d][\w\-_]{{0,}})"
 				: @$"(?:^|[\s\[\({{,])(##\d[\w\-_]{{0,}}|(?!#(?:[A-Fa-f0-9]{{6}}|define|else|endif|endregion|error|include|if|ifdef|ifndef|line|pragma|region|undef)(?:\s|$|\)|\]|}}|[^\w\d\-_]))#{{{count}}}[^\W\d][\w\-_]{{0,}})",
 				RegexOptions.Compiled | RegexOptions.CultureInvariant
 				);
-
-			this.styleTemplate = styleTemplate;
 		}
 
 
