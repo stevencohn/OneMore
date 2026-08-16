2d49d368e0ed34e2edec5692a4ca87174b7a92e0
stevencohn <stevencohn@live.com>
2026-08-16 18:32:35 -0400

    File Import Sheet UI

--- OneMore/Commands/Settings/FileImportSheet.Designer.cs
+++ OneMore/Commands/Settings/FileImportSheet.Designer.cs
@@ -46,23 +46,23 @@ namespace River.OneMoreAddIn.Settings
 			this.ppGroup.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.widthBox)).BeginInit();
 			this.SuspendLayout();
-			//
+			// 
 			// introBox
-			//
+			// 
 			this.introBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
 			this.introBox.Dock = System.Windows.Forms.DockStyle.Top;
 			this.introBox.ForeColor = System.Drawing.SystemColors.ControlText;
 			this.introBox.Location = new System.Drawing.Point(13, 8);
 			this.introBox.Name = "introBox";
 			this.introBox.Padding = new System.Windows.Forms.Padding(0, 0, 0, 36);
-			this.introBox.ThemedBack = "ControlLightLight";
-			this.introBox.ThemedFore = "ControlText";
 			this.introBox.Size = new System.Drawing.Size(772, 66);
 			this.introBox.TabIndex = 3;
 			this.introBox.Text = "File Import Options";
-			//
+			this.introBox.ThemedBack = "ControlLightLight";
+			this.introBox.ThemedFore = "ControlText";
+			// 
 			// layoutPanel
-			//
+			// 
 			this.layoutPanel.Controls.Add(this.quickGroup);
 			this.layoutPanel.Controls.Add(this.ppGroup);
 			this.layoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -71,39 +71,41 @@ namespace River.OneMoreAddIn.Settings
 			this.layoutPanel.Name = "layoutPanel";
 			this.layoutPanel.Size = new System.Drawing.Size(772, 416);
 			this.layoutPanel.TabIndex = 4;
-			//
+			// 
 			// quickGroup
-			//
-			this.quickGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-			| System.Windows.Forms.AnchorStyles.Right)));
+			// 
+			this.quickGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.quickGroup.Controls.Add(this.sectionLink);
 			this.quickGroup.Controls.Add(this.sectionLabel);
 			this.quickGroup.Controls.Add(this.folderButton);
 			this.quickGroup.Controls.Add(this.folderBox);
 			this.quickGroup.Controls.Add(this.folderLabel);
 			this.quickGroup.Controls.Add(this.quickIntroLabel);
-			this.quickGroup.Location = new System.Drawing.Point(3, 100);
+			this.quickGroup.Location = new System.Drawing.Point(3, 110);
 			this.quickGroup.Name = "quickGroup";
 			this.quickGroup.Padding = new System.Windows.Forms.Padding(10, 3, 3, 3);
 			this.quickGroup.ShowOnlyTopEdge = true;
-			this.quickGroup.Size = new System.Drawing.Size(766, 240);
+			this.quickGroup.Size = new System.Drawing.Size(766, 230);
 			this.quickGroup.TabIndex = 6;
 			this.quickGroup.TabStop = false;
 			this.quickGroup.Text = "Quick Import";
 			this.quickGroup.ThemedBorder = null;
 			this.quickGroup.ThemedFore = null;
-			//
+			// 
 			// sectionLink
-			//
+			// 
+			this.sectionLink.Active = false;
 			this.sectionLink.ActiveLinkColor = System.Drawing.Color.MediumOrchid;
 			this.sectionLink.AutoSize = true;
 			this.sectionLink.Cursor = System.Windows.Forms.Cursors.Hand;
 			this.sectionLink.HoverColor = System.Drawing.Color.MediumOrchid;
 			this.sectionLink.LinkColor = System.Drawing.Color.MediumOrchid;
-			this.sectionLink.Location = new System.Drawing.Point(13, 195);
+			this.sectionLink.Location = new System.Drawing.Point(124, 152);
 			this.sectionLink.Name = "sectionLink";
+			this.sectionLink.NavMode = false;
 			this.sectionLink.Selected = false;
-			this.sectionLink.Size = new System.Drawing.Size(160, 20);
+			this.sectionLink.Size = new System.Drawing.Size(121, 20);
 			this.sectionLink.StrictColors = false;
 			this.sectionLink.TabIndex = 5;
 			this.sectionLink.TabStop = true;
@@ -112,22 +114,22 @@ namespace River.OneMoreAddIn.Settings
 			this.sectionLink.ThemedFore = null;
 			this.sectionLink.VisitedLinkColor = System.Drawing.Color.MediumOrchid;
 			this.sectionLink.Click += new System.EventHandler(this.SelectSection);
-			//
+			// 
 			// sectionLabel
-			//
+			// 
 			this.sectionLabel.AutoSize = true;
-			this.sectionLabel.Location = new System.Drawing.Point(13, 170);
+			this.sectionLabel.Location = new System.Drawing.Point(13, 152);
 			this.sectionLabel.Name = "sectionLabel";
-			this.sectionLabel.Size = new System.Drawing.Size(64, 20);
+			this.sectionLabel.Size = new System.Drawing.Size(63, 20);
 			this.sectionLabel.TabIndex = 4;
 			this.sectionLabel.Text = "Section";
-			//
+			// 
 			// folderButton
-			//
+			// 
 			this.folderButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(231)))));
 			this.folderButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
 			this.folderButton.ImageOver = null;
-			this.folderButton.Location = new System.Drawing.Point(690, 108);
+			this.folderButton.Location = new System.Drawing.Point(717, 100);
 			this.folderButton.Name = "folderButton";
 			this.folderButton.ShowBorder = true;
 			this.folderButton.Size = new System.Drawing.Size(36, 31);
@@ -138,59 +140,59 @@ namespace River.OneMoreAddIn.Settings
 			this.folderButton.ThemedFore = null;
 			this.folderButton.UseVisualStyleBackColor = true;
 			this.folderButton.Click += new System.EventHandler(this.BrowseFolder);
-			//
+			// 
 			// folderBox
-			//
+			// 
 			this.folderBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.folderBox.Location = new System.Drawing.Point(13, 109);
+			this.folderBox.Location = new System.Drawing.Point(128, 101);
 			this.folderBox.Name = "folderBox";
 			this.folderBox.ProcessEnterKey = false;
-			this.folderBox.Size = new System.Drawing.Size(670, 26);
+			this.folderBox.Size = new System.Drawing.Size(582, 26);
 			this.folderBox.TabIndex = 2;
 			this.folderBox.ThemedBack = null;
 			this.folderBox.ThemedFore = null;
-			//
+			// 
 			// folderLabel
-			//
+			// 
 			this.folderLabel.AutoSize = true;
-			this.folderLabel.Location = new System.Drawing.Point(13, 84);
+			this.folderLabel.Location = new System.Drawing.Point(13, 105);
 			this.folderLabel.Name = "folderLabel";
-			this.folderLabel.Size = new System.Drawing.Size(58, 20);
+			this.folderLabel.Size = new System.Drawing.Size(54, 20);
 			this.folderLabel.TabIndex = 1;
 			this.folderLabel.Text = "Folder";
-			//
+			// 
 			// quickIntroLabel
-			//
-			this.quickIntroLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-			| System.Windows.Forms.AnchorStyles.Right)));
-			this.quickIntroLabel.Location = new System.Drawing.Point(13, 27);
+			// 
+			this.quickIntroLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.quickIntroLabel.Location = new System.Drawing.Point(13, 44);
 			this.quickIntroLabel.Name = "quickIntroLabel";
 			this.quickIntroLabel.Size = new System.Drawing.Size(740, 45);
 			this.quickIntroLabel.TabIndex = 0;
 			this.quickIntroLabel.Text = "These options enable the quick-import feature";
 			this.quickIntroLabel.ThemedBack = null;
 			this.quickIntroLabel.ThemedFore = null;
-			//
+			// 
 			// ppGroup
-			//
-			this.ppGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-			| System.Windows.Forms.AnchorStyles.Right)));
+			// 
+			this.ppGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.ppGroup.Controls.Add(this.widthBox);
 			this.ppGroup.Controls.Add(this.widthLabel);
 			this.ppGroup.Location = new System.Drawing.Point(3, 6);
 			this.ppGroup.Name = "ppGroup";
 			this.ppGroup.Padding = new System.Windows.Forms.Padding(10, 3, 3, 3);
 			this.ppGroup.ShowOnlyTopEdge = true;
-			this.ppGroup.Size = new System.Drawing.Size(766, 80);
+			this.ppGroup.Size = new System.Drawing.Size(766, 98);
 			this.ppGroup.TabIndex = 5;
 			this.ppGroup.TabStop = false;
 			this.ppGroup.Text = "PowerPoint and PDF";
 			this.ppGroup.ThemedBorder = null;
 			this.ppGroup.ThemedFore = null;
-			//
+			// 
 			// widthBox
-			//
-			this.widthBox.Location = new System.Drawing.Point(347, 33);
+			// 
+			this.widthBox.Location = new System.Drawing.Point(347, 41);
 			this.widthBox.Maximum = new decimal(new int[] {
             2000,
             0,
@@ -209,18 +211,18 @@ namespace River.OneMoreAddIn.Settings
             0,
             0,
             0});
-			//
+			// 
 			// widthLabel
-			//
+			// 
 			this.widthLabel.AutoSize = true;
-			this.widthLabel.Location = new System.Drawing.Point(13, 35);
+			this.widthLabel.Location = new System.Drawing.Point(13, 43);
 			this.widthLabel.Name = "widthLabel";
 			this.widthLabel.Size = new System.Drawing.Size(211, 20);
 			this.widthLabel.TabIndex = 0;
 			this.widthLabel.Text = "Preferred import image width";
-			//
+			// 
 			// FileImportSheet
-			//
+			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
