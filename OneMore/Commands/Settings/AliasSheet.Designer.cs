3f481204f6a06f5bc8945276daac35139e81da6d
Steven <stevencohn@users.noreply.github.com>
2026-08-22 15:19:10 -0400

    2492 filter alias sheet (#2527)
    Relates to #2492

--- OneMore/Commands/Settings/AliasSheet.Designer.cs
+++ OneMore/Commands/Settings/AliasSheet.Designer.cs
@@ -32,7 +32,12 @@
 			this.cmdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.aliasColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.introBox = new River.OneMoreAddIn.UI.MoreMultilineLabel();
+			this.filterPanel = new River.OneMoreAddIn.UI.MorePanel();
+			this.filterLabel = new River.OneMoreAddIn.UI.MoreLabel();
+			this.filterBox = new River.OneMoreAddIn.UI.MoreTextBox();
+			this.filterClearButton = new River.OneMoreAddIn.UI.MoreButton();
 			((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+			this.filterPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// gridView
@@ -46,14 +51,14 @@
             this.cmdColumn,
             this.aliasColumn});
 			this.gridView.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.gridView.Location = new System.Drawing.Point(10, 65);
+			this.gridView.Location = new System.Drawing.Point(10, 104);
 			this.gridView.MultiSelect = false;
 			this.gridView.Name = "gridView";
 			this.gridView.RowHeadersVisible = false;
 			this.gridView.RowHeadersWidth = 30;
 			this.gridView.RowTemplate.Height = 28;
 			this.gridView.ShowEditingIcon = false;
-			this.gridView.Size = new System.Drawing.Size(780, 426);
+			this.gridView.Size = new System.Drawing.Size(780, 387);
 			this.gridView.TabIndex = 2;
 			this.gridView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.ValidateAlias);
 			// 
@@ -88,13 +93,68 @@
 			this.introBox.Size = new System.Drawing.Size(780, 56);
 			this.introBox.TabIndex = 0;
 			this.introBox.Text = "Define command aliases for use in the Command Palette";
-			// 
+			//
+			// filterPanel
+			//
+			this.filterPanel.BottomBorderSize = 0;
+			this.filterPanel.Controls.Add(this.filterClearButton);
+			this.filterPanel.Controls.Add(this.filterBox);
+			this.filterPanel.Controls.Add(this.filterLabel);
+			this.filterPanel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.filterPanel.Location = new System.Drawing.Point(10, 65);
+			this.filterPanel.Name = "filterPanel";
+			this.filterPanel.Size = new System.Drawing.Size(780, 39);
+			this.filterPanel.TabIndex = 1;
+			this.filterPanel.ThemedBack = "ControlLightLight";
+			this.filterPanel.TopBorderSize = 0;
+			//
+			// filterLabel
+			//
+			this.filterLabel.AutoSize = true;
+			this.filterLabel.Location = new System.Drawing.Point(0, 9);
+			this.filterLabel.Name = "filterLabel";
+			this.filterLabel.Size = new System.Drawing.Size(48, 20);
+			this.filterLabel.TabIndex = 0;
+			this.filterLabel.Text = "Filter";
+			//
+			// filterBox
+			//
+			this.filterBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.filterBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.filterBox.Location = new System.Drawing.Point(60, 4);
+			this.filterBox.Name = "filterBox";
+			this.filterBox.ProcessEnterKey = true;
+			this.filterBox.Size = new System.Drawing.Size(680, 26);
+			this.filterBox.TabIndex = 1;
+			this.filterBox.ThemedBack = null;
+			this.filterBox.ThemedFore = null;
+			this.filterBox.TextChanged += new System.EventHandler(this.FilterCommands);
+			//
+			// filterClearButton
+			//
+			this.filterClearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.filterClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.filterClearButton.ImageOver = null;
+			this.filterClearButton.Location = new System.Drawing.Point(750, 2);
+			this.filterClearButton.Name = "filterClearButton";
+			this.filterClearButton.ShowBorder = false;
+			this.filterClearButton.Size = new System.Drawing.Size(30, 30);
+			this.filterClearButton.StylizeImage = false;
+			this.filterClearButton.TabIndex = 2;
+			this.filterClearButton.Text = "✕";
+			this.filterClearButton.ThemedBack = null;
+			this.filterClearButton.ThemedFore = null;
+			this.filterClearButton.UseVisualStyleBackColor = true;
+			this.filterClearButton.Click += new System.EventHandler(this.ClearFilter);
+			//
 			// AliasSheet
-			// 
+			//
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
 			this.Controls.Add(this.gridView);
+			this.Controls.Add(this.filterPanel);
 			this.Controls.Add(this.introBox);
 			this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
 			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -103,6 +163,8 @@
 			this.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
 			this.Size = new System.Drawing.Size(800, 500);
 			((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+			this.filterPanel.ResumeLayout(false);
+			this.filterPanel.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -111,6 +173,10 @@
 		#endregion
 		private UI.MoreDataGridView gridView;
 		private UI.MoreMultilineLabel introBox;
+		private UI.MorePanel filterPanel;
+		private UI.MoreLabel filterLabel;
+		private UI.MoreTextBox filterBox;
+		private UI.MoreButton filterClearButton;
 		private System.Windows.Forms.DataGridViewTextBoxColumn cmdColumn;
 		private System.Windows.Forms.DataGridViewTextBoxColumn aliasColumn;
 	}
