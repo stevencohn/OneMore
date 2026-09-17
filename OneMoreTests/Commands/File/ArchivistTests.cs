dc8bf949728ce9a1390f3e394dc87bfebea70586
Steven <stevencohn@users.noreply.github.com>
2026-09-16 20:39:02 -0400

    2648 link footnotes on archive (#2654)
    Relates to #2648

--- OneMoreTests/Commands/File/ArchivistTests.cs
+++ OneMoreTests/Commands/File/ArchivistTests.cs
@@ -65,5 +65,98 @@ namespace River.OneMoreAddIn.Tests.Commands.File
 
 			Assert.IsTrue(result.StartsWith("<H1 id=\"boxes\">"));
 		}
+
+
+		// realistic onenote: href, carrying section-id/page-id/object-id - a naive
+		// "already has an id" check (bare \bid\s*=) false-matches the "-id=" in these
+		// attribute names and must not; see InjectFootnoteAnchors_WithFooterBacklink_AddsOmfnId
+		private const string OnenoteHref =
+			"onenote:#Page&amp;section-id={A640CEA0-536E-4ED0-ACC1-428AAB96501F}&amp;" +
+			"page-id={660B56BC-B6BE-4791-B556-E4BC9BA2E60C}&amp;" +
+			"object-id={3F8BE067-A5EA-4025-8F40-CDFD5DC1F59F}&amp;end";
+
+
+		[TestMethod]
+		public void InjectFootnoteAnchors_WithRefMarker_AddsOmfnrefId()
+		{
+			var html =
+				$"<a href=\"{OnenoteHref}\"><span style=\"vertical-align:super\">[1]</span></a>";
+
+			var result = Archivist.InjectFootnoteAnchorsInHtml(html);
+
+			Assert.AreEqual(
+				$"<a href=\"{OnenoteHref}\" id=\"omfnref-1\">" +
+				"<span style=\"vertical-align:super\">[1]</span></a>",
+				result);
+		}
+
+
+		[TestMethod]
+		public void InjectFootnoteAnchors_WithFooterBacklink_AddsOmfnId()
+		{
+			var html =
+				$"<a href=\"{OnenoteHref}\">" +
+				"<span style=\"font-family:'Calibri Light';font-size:11.0pt\">[1]</span></a>";
+
+			var result = Archivist.InjectFootnoteAnchorsInHtml(html);
+
+			Assert.AreEqual(
+				$"<a href=\"{OnenoteHref}\" id=\"omfn-1\">" +
+				"<span style=\"font-family:'Calibri Light';font-size:11.0pt\">[1]</span></a>",
+				result);
+		}
+
+
+		[TestMethod]
+		public void InjectFootnoteAnchors_WithExistingId_LeavesUntouched()
+		{
+			var html = "<a href=\"\" id=\"already-set\"><span style=\"vertical-align:super\">[1]</span></a>";
+
+			var result = Archivist.InjectFootnoteAnchorsInHtml(html);
+
+			Assert.AreEqual(html, result);
+		}
+
+
+		[TestMethod]
+		public void InjectFootnoteAnchors_WithOrdinaryLink_AddsNoId()
+		{
+			var html = "<a href=\"./Other.htm\">Some other page</a>";
+
+			var result = Archivist.InjectFootnoteAnchorsInHtml(html);
+
+			Assert.AreEqual(html, result);
+		}
+
+
+		[TestMethod]
+		public void ResolveFootnoteAnchor_WithRefMarkerText_ReturnsOmfnId()
+		{
+			var linkText = "<span style=\"vertical-align:super\">[1]</span>";
+
+			var result = Archivist.ResolveFootnoteAnchor(linkText);
+
+			Assert.AreEqual("omfn-1", result);
+		}
+
+
+		[TestMethod]
+		public void ResolveFootnoteAnchor_WithBacklinkText_ReturnsOmfnrefId()
+		{
+			var linkText = "<span style=\"font-family:'Calibri Light';font-size:11.0pt\">[1]</span>";
+
+			var result = Archivist.ResolveFootnoteAnchor(linkText);
+
+			Assert.AreEqual("omfnref-1", result);
+		}
+
+
+		[TestMethod]
+		public void ResolveFootnoteAnchor_WithOrdinaryText_ReturnsNull()
+		{
+			var result = Archivist.ResolveFootnoteAnchor("Groovy Page");
+
+			Assert.IsNull(result);
+		}
 	}
 }
